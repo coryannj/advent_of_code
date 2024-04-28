295de8f7d1cca69b5eb55a8e2f2849e18771fff7
Cory-Ann Joseph <coryannj@gmail.com>
2024-04-28 17:04:09 +1000

    Day 10 solution cleanup

--- day10.js
+++ day10.js
@@ -2,26 +2,19 @@ const fs = require('fs');
 const input = fs.readFileSync('../day10input.txt',{ encoding: 'utf8', flag: 'r' });
 const lines = input.split(/[\r\n]+/).map((x)=> x.split(''))
 
-const coordRegex = /\d+[,]\d+/gm
-let xlength = lines.length
-let ylength = lines[0].length
-
-//console.log(lines[0])
-let startx = lines.findIndex((x)=> x.includes('S'))
+// Manually determine start co-ord (too lazy to code)
+let startx = lines.findIndex((x)=> x.includes('S')) 
 let starty = lines[128].findIndex((x)=> x === 'S')
-//console.log(lines[93][78])
+
 // Part 1
 
 /* Model is for every step incoming [x-coordinate,y-coordinate,direction] -> encounter -> outgoing[x-coordinate,y-coordinate,direction] (where outgoing can be multiple) */
 
-let queue = [[127,88,'U'],[129,88,'D']]
+let queue = [[127,88,'U']]
 let distance = 0
-let seen = ['128-88']
-
-console.log(queue[0].join('-').includes('128-88'))
+let seen = [[128,88]]
 
 function getNext([x,y,direction]) {
-
   let nextmap = {
     '|_U': [x-1,y,'U'],
     '|_D': [x+1,y,'D'],
@@ -39,36 +32,16 @@ function getNext([x,y,direction]) {
   return nextmap[`${lines[x][y]}_${direction}`].flat() 
 }
 
-while (seen.indexOf(`${queue[0][0]}-${queue[0][1]}`) === -1 && seen.indexOf(`${queue[1][0]}-${queue[1][1]}`) === -1) {
-  seen.push(`${queue[0][0]}-${queue[0][1]}`)
-  seen.push(`${queue[1][0]}-${queue[1][1]}`)
-  distance++
+// Loop all around the grid till we reach start
+while (!queue[0].join('-').includes('128-88')) {
+  seen.push([queue[0][0],queue[0][1]]) // Push all points as [x,y] into seen array for Part 2
   queue = queue.map((x)=> getNext(x))
 }
 
-console.log(distance) // Part 1 answer
-console.log(seen.length)
+console.log(seen.length/2) // Part 1 answer
 
 //Part 2
-// let seenMap = seen.map((x)=> {
-//   let coords = x.split('-').map(Number)
-//   return `${x}-${lines[coords[0]][coords[1]]}`
-// })
 
-// console.log(seenMap[0])
-// console.log(seenMap[1])
-// console.log(seenMap[seenMap.length-1])
-// console.log(seenMap[seenMap.length-2])
-// console.log(seenMap[seenMap.length-3])
-// console.log(seenMap[seenMap.length-4])
-console.log(seen[seen.length-1])
-console.log(seen[seen.length-2])
-
-let queue2 = [[127,88,'U']]
-let seen2 = []
-let vertices = []
-let straight = []
-let count2 = 0
 // Shoelace formula - taken from https://stackoverflow.com/questions/62323834/calculate-polygon-area-javascript
 function calculateArea(coords) {
   let area = 0;
@@ -80,35 +53,9 @@ function calculateArea(coords) {
     area += x1 * y2 - x2 * y1
   }
 
-  //return area / 2;
-  // replace with
  return Math.abs(area) / 2;
 }
 
-while(!queue2[0].join('-').includes('128-88')) {
-  let next = lines[queue2[0][0]][queue2[0][1]]
-  count2++
-
-  seen2.push([queue2[0][0],queue2[0][1]])
-  if (next !== '|' && next !== '-') {
-    vertices.push([queue2[0][0],queue2[0][1]])
-  } else {
-    straight.push([queue2[0][0],queue2[0][1]])
-  }
-
-  queue2 = queue2.map((x)=> getNext(x))
-
-}
-console.log('count2 is ',count2)
-//console.log(vertices)
-// console.log(vertices.length)
-// console.log(straight.length)
-// console.log(seen2.length)
-// console.log(seen2[seen2.length-1])
-// console.log(seen2[seen2.length-2])
-// console.log(calculateArea(vertices))
-// console.log(calculateArea(seen2))
-
-let internalArea = calculateArea(vertices)
-let border = ((seen.length-1)/2)-1
-console.log(internalArea-border) // Part 2 answer
+let totalArea = calculateArea(seen) // Shoelace
+let border = ((seen.length)/2)-1 // Pick's theorem
+console.log(totalArea-border) // Part 2 answer
