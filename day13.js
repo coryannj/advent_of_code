da13ffc997307970e9808fcd6043bfb55b637738
Cory-Ann Joseph <coryannj@gmail.com>
2024-05-17 01:33:40 +1000

    Day 13 refactor

--- day13.js
+++ day13.js
@@ -3,121 +3,89 @@ const input = fs.readFileSync('../day13input.txt',{ encoding: 'utf8', flag: 'r' 
 
 const mirrors = input.split(/$[\r\n]{1,2}^$[\r\n]{1,2}^/m)
 
-function rowDifference(row1,row2) {
-  return row1.split('').map((x,idx)=> row2.charAt(idx) === x ? 0 : 1).reduce((acc,curr)=> acc+curr)
+function getDifference(rowcol1,rowcol2) {
+  return rowcol1.split('').map((x,idx)=> rowcol2.charAt(idx) === x ? 0 : 1).reduce((acc,curr)=> acc+curr)
 }
 
-//Part 1
-let p1total=0 
+function findReflections(allMirrors,p1orp2) {
+  let total = 0
+  let allowedDifference
 
-mirrors.forEach((mirror,mirroridx)=> {
-  var rows = mirror.split(/[\r\n]+/)
-  var xlength = rows.length
-  var found = false
+  p1orp2 === 'part1' ? allowedDifference = 0 : allowedDifference = 1
 
-  for (i=0;i<xlength-1;i++){
-    if (rows[i] === rows[i+1]) {
-      var notMatching = 0
-      var r1 = i
-      var r2 = i+1
-      while(r1 > 0 && r2 < xlength-1) {
-        r1--
-        r2++
-        notMatching += rowDifference(rows[r1],rows[r2])
+  allMirrors.forEach((mirror)=> {
+    var rows = mirror.split(/[\r\n]+/)
+    var xlength = rows.length
+    var found = false
+    var rowCheck
+    
+    for (i=0;i<xlength-1;i++){ // Check rows
+
+      if (p1orp2 === 'part1') {
+        rowCheck = rows[i] === rows[i+1]
+      } else {
+        rowCheck = (rows[i] === rows[i+1] || getDifference(rows[i],rows[i+1]) === 1)
       }
 
-      if (notMatching === 0) {
-        p1total+=((i+1)*100)
-        found=true
-        break;
-      }
-    }
-  }
-
-  if (found === false) {
-    var colSplit = rows.map((x)=> x.split(''))
-    var cols = colSplit[0].map((val, index) => colSplit.map(row => row[index]).join(''));
-    var ylength = cols.length
-
-    for (i=0;i<ylength-1;i++){
-      if (cols[i] === cols[i+1]) {
-        var notMatching = 0
-        var c1 = i
-        var c2 = i+1
-        while(c1 > 0 && c2 < ylength-1) {
-          c1--
-          c2++
-          notMatching += rowDifference(cols[c1],cols[c2])
-        }
-
-        if (notMatching === 0) {
-          p1total+=i+1
-          found=true
-          break;
-        }
-      }
-    }
-  }
-
-})
-
-console.log(p1total) // Part 1 answer
-
-//Part 2
-let p2total = 0
-
-mirrors.forEach((mirror,mirroridx)=> {
-  var rows = mirror.split(/[\r\n]+/)
-  var xlength = rows.length
-  var found = false
-
-  for (i=0;i<xlength-1;i++){
-    if (rows[i] === rows[i+1] || rowDifference(rows[i],rows[i+1]) === 1) {
-      var notMatching
-      rowDifference(rows[i],rows[i+1]) === 1 ? notMatching=1 : notMatching=0
-      var r1 = i
-      var r2 = i+1
-      while(r1 > 0 && r2 < xlength-1) {
-        r1--
-        r2++
-        notMatching += rowDifference(rows[r1],rows[r2])
-      }
-
-      if (notMatching === 1) {
-        p2total+=((i+1)*100)
-        found=true
-        break;
-      }
-    }
-  }
-
-  if (found === false) {
-    var colSplit = rows.map((x)=> x.split(''))
-    var cols = colSplit[0].map((val, index) => colSplit.map(row => row[index]).join(''));
-    var ylength = cols.length
-
-    for (i=0;i<ylength-1;i++){
-      if (cols[i] === cols[i+1] || rowDifference(cols[i],cols[i+1]) === 1) {
+      if (!!rowCheck) {
         var notMatching
-        rowDifference(cols[i],cols[i+1]) === 1 ?notMatching=1 : notMatching=0
-        var c1 = i
-        var c2 = i+1
-        while(c1 > 0 && c2 < ylength-1) {
-          c1--
-          c2++
-          notMatching += rowDifference(cols[c1],cols[c2])
-        }
+        p1orp2 === 'part2' && getDifference(rows[i],rows[i+1]) === 1 ? notMatching=1 : notMatching=0
 
-        if (notMatching === 1) {
-          p2total+=i+1
-          found=true
+        var r1 = i
+        var r2 = i+1
+        while(r1 > 0 && r2 < xlength-1) {
+          r1--;
+          r2++;
+          notMatching += getDifference(rows[r1],rows[r2]);
+        }
+  
+        if (notMatching === allowedDifference) {
+          total+=((i+1)*100);
+          found=true;
           break;
         }
       }
     }
-  }
+  
+    if (found === false) { // Check columns
+      
+      var colSplit = rows.map((x)=> x.split(''));
+      var cols = colSplit[0].map((val, index) => colSplit.map(row => row[index]).join(''));
+      var ylength = cols.length;
+      var colCheck
 
-})
+      for (i=0;i<ylength-1;i++){
+        if (p1orp2 === 'part1') {
+          colCheck = cols[i] === cols[i+1]
+        } else {
+          colCheck = (cols[i] === cols[i+1] || getDifference(cols[i],cols[i+1]) === 1)
+        }
 
-console.log(p2total) // Part 2 answer
+        if (!!colCheck) {
+          var notMatching
+          p1orp2 === 'part2' && getDifference(cols[i],cols[i+1]) === 1 ? notMatching=1 : notMatching=0;
+  
+          var c1 = i;
+          var c2 = i+1;
+          while(c1 > 0 && c2 < ylength-1) {
+            c1--;
+            c2++;
+            notMatching += getDifference(cols[c1],cols[c2]);
+          }
+  
+          if (notMatching === allowedDifference) {
+            total+=i+1;
+            found=true;
+            break;
+          }
+        }
+      }
+    }
+  
+  })
 
+  return total
+}
+
+console.log(findReflections(mirrors,'part1')) // Part 1 answer
+console.log(findReflections(mirrors,'part2')) // Part 2 answer
