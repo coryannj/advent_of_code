a2117008b6d6aecf017c5fc0765dfa952165777d
Cory-Ann Joseph <coryannj@gmail.com>
2024-06-06 02:27:24 +1000

    Day 12 cleanup

--- day12.js
+++ day12.js
@@ -4,186 +4,149 @@ const input = fs.readFileSync('../day12input.txt',{ encoding: 'utf8', flag: 'r' 
 
 let lines = input.split(/[\r\n]+/).map((x)=> {let [a,b]=x.split(' '); return [a,b.split(',').map((n)=>parseInt(n))]})
 
-function getArrangements (spr,br,p1orp2) {
+/*
+  Solution based on https://github.com/clrfl/AdventOfCode2023/blob/master/12/explanation.ipynb
+  (There are quite a few DFA solutions but this was the only one I could understand)
+  
+  ***** Part 1 - Example walkthrough *****
 
+  line = .?#.??#???. 1,2,1
+
+  ** STEP 1 - Trim extra '.' **
+  Remove leading and trailing '.', replace runs of multiple dots e.g. '...' -> '.'
+
+  -> ?#.??#??? 1,2,1
+
+  ** STEP 2 - Create array of all possible states **
+  1,2,1 in regex form is ([.]*#[.]+##[.]+#[.])
+
+             states index:  [ 0    1    2    3    4    5    6    7 ]
+  states array looks like:  ['.', '#', '.', '#', '#', '.', '#', '.']
+
+  indexes [0,1] are starting states, [6,7] are end states
+
+  ** STEP 3 - Traverse string and count all possible states at each index **
+    
+    stateidx| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 |
+        +---+---+---+---+---+---+---+---+---+
+     states | . | # | . | # | # | . | # | . | stateCount
+        +---+---+---+---+---+---+---+---+---+
+        | ? | 1   1                         | {0:1,1:1} Special case - populate valid start states with 1
+        +---+---+---+---+---+---+---+---+---+
+        | # | -   1*  -                     | {1:1} State 0 to 1 is valid, but State 1 to 2 ('.') is not 
+        +---+---+---+---+---+---+---+---+---+       as current char is '#'
+        | . |         1                     | {2:1}
+        +---+---+---+---+---+---+---+---+---+
+          ? |         1   1                 | {2:1,3:1} -> State 2 can repeat, and also go to State 3
+        +---+---+---+---+---+---+---+---+---+
+          ? |         1   1   1             | {2:1,3:1,4:1}
+        +---+---+---+---+---+---+---+---+---+
+          # |         -   1*  1   -         | {3:1,4:1} -> State 2 can't repeat, State 4 to 5 is not valid
+        +---+---+---+---+---+---+---+---+---+
+          ? |                 1   1         | {4:1,5:1}
+        +---+---+---+---+---+---+---+---+---+
+          ? |                     2   1     | {5:2,6:1} -> State 5 repeats, State 4 also goes to State 5
+        +---+---+---+---+---+---+---+---+---+
+          ? |                     -   2   1 | {6:2,7:1} -> Count end states -> 3 arrangements
+        +---+---+---+---+---+---+---+---+---+
+
+        *Any time a '#' state which is the first in a broken group coincides with a '#' in the string - add to consumed list as this is the last index it can be seen at. e.g. can't have broken springs before State 1
+
+        Other non-advancing conditions
+        - State index is less than highest consumed index
+        - No more wriggle room - where wriggle = springs.length - (sum of broken + broken.length)
+  
+*/
+
+function getArrangements (spr,br,p1orp2) {
   let springsSplit
   let brokenMap
   if (p1orp2 === 'p1') {
-    springsSplit = spr.split('')
+    springsSplit = spr.replaceAll(/[.]+/g,'.').replaceAll(/(^[.]+|[.]+$)/gm,'').split('')
     brokenMap = br.map((x)=> '#'.repeat(x)).join('.').split('')
   } else {
-    let unfolded = Array(5).fill(spr).join('?').replaceAll(/[.]+/g,'.').replaceAll(/^[.]+/gm,'').replaceAll(/[.]+\s/gm,' ')
-
-    unfolded = unfolded.replaceAll(/[.]+/g,'.').replaceAll(/^[.]+/gm,'').replaceAll(/[.]+\s/gm,' ')
-
-    springsSplit = unfolded.split('')
+    springsSplit = Array(5).fill(spr).join('?').replaceAll(/[.]+/g,'.').replaceAll(/(^[.]+|[.]+$)/gm,'').split('')
     brokenMap = Array(5).fill(br.join(',')).flatMap((x)=> x.split(',').map((y)=> parseInt(y))).map((z)=> '#'.repeat(z)).join('.').split('')
   }
 
-  let wriggle = springsSplit.length-brokenMap.length
-
-  let states = ['.'].concat(brokenMap,'.')
-  let statesCount = []
-  let consumedIndex = []
+  let sprLength = springsSplit.length;
+  let wriggle = sprLength-brokenMap.length;
+  let states = ['.'].concat(brokenMap,'.');
+  let endStateIdx = states.length-1;
+  let secondLastStateIdx = endStateIdx-1;
+  let statesCount = new Map();
+  let consumedIndex = [];
   
-  // console.log(' *** FUNCTION ***')
-  // console.log('spr,br ',spr,br)
-  // console.log('springsSplit is ',springsSplit)
-  // console.log('brokenMap is ',brokenMap)
-  // console.log('wriggle',wriggle)
-  // console.log(' STATES ')
-  // console.log(states)
-  // console.log(' ')
+  // Set starting states
+  statesCount.set(0,1)
+  statesCount.set(1,1)
 
-  springsSplit.forEach((spring,spridx) =>{
-    // console.log('spring is ',spring)
-    // console.log('spridx is ',spridx)
-    let consumedSet =false
-    if (spridx === 0) {
+  if (springsSplit.at(0) === '#') { 
+    statesCount.delete(0);
+    consumedIndex.push(1);
+  }
 
-      if (spring === '#') {
-        statesCount.push({1:1})
-        consumedIndex.push(1)
-      } else if (spring === '.') {
-        statesCount.push({0:1})
-      } else {
-        statesCount.push({0:1,1:1})
-      }
+  if (springsSplit.at(0) === '.') {
+    statesCount.delete(1);
+  } 
 
-    } else if (spridx > 0 && spridx < springsSplit.length) {
-      let nextObj={}
-      for([idx,val]of Object.entries(statesCount[spridx-1])){
-          //console.log('idx,val is ',idx,val)
-        //let statemin = (springsSplit.length-1) - (brokemin-idx)
+  for(i=1;i<sprLength;i++) { // Loop through springs
+    let nextStates=new Map();
+    let spring = springsSplit.at(i)
+    let spridx = i
 
-        let idxNum = parseInt(idx)
-        let lastState = states[idxNum]
-        let nextState = states[idxNum+1]
-        let wriggleIndex = idxNum+wriggle
-        let nextVal = val
-        // console.log('last state ',lastState)
-        // console.log('next state ',nextState)
-        // console.log('wriggleIndex is ',wriggleIndex)
-        let nextStateVal
+    statesCount.forEach((stateVal, stateIdx) => { // Loop through previous states
+      let wriggleIndex = stateIdx+wriggle;
+      let prevState = states[stateIdx];
+      let nextState = states[stateIdx+1];
 
-        if (statesCount[spridx-1][idxNum+1] === undefined) {
-          nextStateVal=0
-        } else {
-          nextStateVal=statesCount[spridx-1][idxNum+1]
-        }
+      if (spridx<=wriggleIndex && (consumedIndex.length === 0 || stateIdx >= consumedIndex.at(-1))) {
 
+        // Check if next state is valid and advance
+        if (stateIdx<endStateIdx) {
+          if ((nextState === '#' && spring !== '.'|| nextState === '.' && spring !== '#') && (stateIdx<secondLastStateIdx-1||!springsSplit.join('').substring(spridx+1).includes('#'))) {
+            let nextStateVal = 0;
 
-        //idxNum === 0 ? nextVal = 1 : nextVal = val
-        if (spridx<=wriggleIndex && (consumedIndex.length === 0||idxNum>=(consumedIndex.at(-1)))) {
-          //console.log('first if block - spridx<wriggleindex or consumedindex check')
-  
-          if (idxNum < states.length-2) {
-            if (nextState === '#' && spring !== '.') {
-              if(idxNum === states.length-3) {
-                if (!spr.substring(spridx+1).includes('#')) {
-                  nextObj[idxNum+1] = nextVal
-                }
-              } else {
-                nextObj[idxNum+1] = nextVal
-              }
-            
+            if (nextState === '.' && statesCount.has(stateIdx+1)) { 
+              nextStateVal=statesCount.get(stateIdx+1); // Get prev count to add to
             }
-            
-            if (nextState === '.' && spring !== '#') {
-              nextObj[idxNum+1] = nextVal+nextStateVal
-            }
-  
-            if (lastState === '.' && spring !== '#' && statesCount[spridx-1][idxNum-1] === undefined) {
-              nextObj[idxNum] = nextVal
-            }
+
+            nextStates.set(stateIdx+1,stateVal+nextStateVal);
           }
-
-
         }
-        if (idxNum === states.length-2) {
-           //console.log('in first end state block last #')
-           //console.log(!springsSplit.join('').substring(spridx+1).includes('#') && spring !== '#')
-          if(!springsSplit.join('').substring(spridx+1).includes('#') && spring !== '#') {
-            nextObj[idxNum+1] = nextVal+nextStateVal
-          }
-
-        }
-
-        if (idxNum === states.length-1 && statesCount[spridx-1][idxNum-1] === undefined) {
-          nextObj[idxNum] = val
-        }
-
-        if (spring === '#' && consumedSet===false) {
-          let earliestSpring = Object.keys(nextObj).find((x)=> states[parseInt(x)] === '#')
-          //console.log('earliest Spring is - states are # ',earliestSpring)
-          
-          if(earliestSpring !== undefined){
-            if (states[(parseInt(earliestSpring)-1)] === '.') {
-              consumedIndex.push(parseInt(earliestSpring))
-              consumedSet=true
-              //console.log('*** if block = consumed index updated ***')
-            }
-
-            //earliestSpring = earliestSpring.find((y)=> states[(parseInt(y)-1)] === '.')
-            //  console.log('find correct earliest spring ',earliestSpring)
-            // if (earliestSpring !== undefined) {
-            //   consumedIndex.push(parseInt(earliestSpring))
-            //   consumedSet=true
-            //    console.log('*** consumed index updated ***')
-            //    //console.log(consumedIndex)
-            // }
-
-          }
-
-
-
-
-        }
-
         
-
-        //console.log(idx,val)
+        // If prev state is '.' and was not advanced to - check if can repeat
+        if (prevState === '.' && spring !== '#' && !statesCount.has(stateIdx-1)) {
+          nextStates.set(stateIdx,stateVal)
+        }
       }
-      if (spridx<wriggle && spring !== '#' && consumedIndex.length === 0) {
-        nextObj[0] = 1
+      
+
+    });
+
+    // Check if consumedIndex needs to be updated
+    if (spring === '#') {
+      let earliestSpring = [...nextStates.keys()].sort((a,b)=> a-b).find((x)=> states[x] === '#')
+      if (earliestSpring !== undefined && states[earliestSpring-1] === '.') {
+        consumedIndex.push(earliestSpring)
+      }
     }
-       
-      statesCount.push(nextObj)
-      // console.log('nextObj is ',nextObj)
-      // console.log('statesCount is')
-      // console.log(statesCount)
-      // console.log('consumed index is')
-      // console.log(consumedIndex)
-      // console.log('*** END ***')
-    } else {
+    
+    statesCount = nextStates // Update states
 
-    }
-  })
+  }
 
-  let lastObj = statesCount.at(-1)
-  
-  let secondLastState = lastObj[states.length-2] === undefined ? 0 : lastObj[states.length-2]
-  let lastState = lastObj[states.length-1] === undefined ? 0 : lastObj[states.length-1]
-
-
-
-  // console.log('secondLastState',secondLastState)
-  // console.log('Last state', lastState)
-
-  return secondLastState+lastState
+  return [...statesCount.keys()].filter((x)=> x === secondLastStateIdx || x === endStateIdx).map((y)=> statesCount.get(y)).reduce((acc,curr)=> acc+curr,0)
 }
+
 let p1sum = 0
 let p2sum = 0
 
 lines.forEach(([springs,broken])=> {
-  //console.log(springs,' ',broken)
-  let p1arrangements = getArrangements(springs,broken,'p1')
-  let p2arrangements = getArrangements(springs,broken,'p2')
-  p1sum += p1arrangements
-  p2sum += p2arrangements
-  //console.log('arrangements ',arrangements, ' and sum is now ',sum)
+  p1sum += getArrangements(springs,broken,'p1')
+  p2sum += getArrangements(springs,broken,'p2')
 })
-console.log(p1sum)
-console.log(p2sum)
+console.log(p1sum) // Part 1 answer
+console.log(p2sum) // Part 2 answer
 
 
