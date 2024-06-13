1a700eeacd2b1b3f4251afd0b9b5e7dcd4e22778
Cory-Ann Joseph <coryannj@gmail.com>
2024-06-13 19:17:22 +1000

    Day 25 cleanup

--- day25.js
+++ day25.js
@@ -3,6 +3,9 @@ const { default: test } = require('node:test');
 const { mainModule } = require('process');
 const input = fs.readFileSync('../day25input.txt',{ encoding: 'utf8', flag: 'r' });
 
+// Solution from https://www.reddit.com/r/adventofcode/comments/18qbsxs/comment/keuafrl/ and 
+// https://www.reddit.com/r/adventofcode/comments/18qbsxs/comment/kevx4yh/ - use Monte Carlo to iterate over paths between 2 random points, find shortest path, count most seen edges -> top 3 is what we need to cut
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
@@ -11,9 +14,8 @@ let lines = input.split(/[\r\n]+/).map((x,idx)=> {
   return x.replace(':','').split(' ')
 })
 
+// Part 1
 let wiring = new Map()
-
-
 
 lines.forEach((wire)=>{
   let component = wire.at(0)
@@ -33,114 +35,73 @@ lines.forEach((wire)=>{
 })
 
 let bridgeMap = {}
- let allKeys = [...wiring.keys()]
- let allLength = allKeys.length-2
+let allKeys = [...wiring.keys()];
+let allLength = allKeys.length-2;
 
-  let pcounter=0
 
- for(j=0;j<1000;j++){
-  let point1 = allKeys[getRandomInt(allLength)]
-  let point2 = allKeys[getRandomInt(allLength)]
-  //console.log(point1,point2)
-  let allPaths = []
-  let seenp = new Set()
-  seenp.add(point1)
-  let p2pqueue = [[point1]]
-  if (point1 !== point2){
-    while(allPaths.length=== 0 && p2pqueue.length>0){
-      //pcounter++
-      //console.log(pcounter)
-      let next = p2pqueue.shift()
-      //console.log(next)
-      let last = next.at(-1)
-      //console.log(next,last)
-      
-      //console.log('next is ',next)
-      let nextVals = wiring.get(last)
-      //console.log(nextVals)
-      if (nextVals !== undefined){
-        nextVals.forEach((val)=>{
-          if(val !== point2){
-            if(!seenp.has(val)){
-              //let edgeKey = next<val ? `${next}_${val}` : `${val}_${next}`
-              //bridgeMap[edgeKey] = (bridgeMap[edgeKey] ?? 0) + 1;
-              p2pqueue.push(next.concat(val))
-              seenp.add(val)
-              //console.log('queue length',p2pqueue.length)
-              //console.log(p2pqueue)
-            }
-          } else {
-            //console.log('next is ',next)
-            //console.log('val is ',val)
-            allPaths.push(next.concat(val))
-            
-          }
-  
-  
-        })
+for(j=0;j<200;j++){ 
+  let point1 = allKeys[getRandomInt(allLength)];
+  let point2 = allKeys[getRandomInt(allLength)];
+
+  let allPaths = [];
+  let seenp = new Set();
+  seenp.add(point1);
+  let p2pqueue = [[point1]]; // We need to store all nodes in path
+
+  while(allPaths.length === 0 && p2pqueue.length>0){ //Djikstra's for each random pair of points
+    let next = p2pqueue.shift();
+    let last = next.at(-1);
+    let nextVals = wiring.get(last);
+
+    nextVals.forEach((val)=>{
+      if(val !== point2){
+        if(!seenp.has(val)){
+          p2pqueue.push(next.concat(val));
+          seenp.add(val);
+        }
+      } else {
+        allPaths.push(next.concat(val))
       }
-      
-    }
-    //console.log('allpaths ',allPaths)
-    //allPaths.sort((a,b)=> a.length-b.length)
-    let node = allPaths[0]
-    if(allPaths.length>0){
-      for(i=1;i<node.length-1;i++){
-        let thisx = node[i]
-        let prevx = node[i-1]
-        let nextx = node[i+1]
-    
-        let prevKey = thisx<prevx ? `${thisx}_${prevx}` : `${prevx}_${thisx}`
-        let nextKey = thisx<nextx ? `${thisx}_${nextx}` : `${nextx}_${thisx}`
-        bridgeMap[prevKey] = (bridgeMap[prevKey] ?? 0) + 1;
-        bridgeMap[nextKey] = (bridgeMap[nextKey] ?? 0) + 1;
-      }
-    }
-    // let node = [...seenp]
-
+    })
   }
 
+  let path = allPaths[0]
 
- }
+  for(i=1;i<path.length-1;i++){ // Count all edges in shortest path and store in object
+    let thisx = path[i];
+    let prevx = path[i-1];
+    let nextx = path[i+1];
 
- console.log('Top 5 bridges are ')
+    let prevKey = thisx<prevx ? `${thisx}_${prevx}` : `${prevx}_${thisx}`
+    let nextKey = thisx<nextx ? `${thisx}_${nextx}` : `${nextx}_${thisx}`
+    bridgeMap[prevKey] = (bridgeMap[prevKey] ?? 0) + 1;
+    bridgeMap[nextKey] = (bridgeMap[nextKey] ?? 0) + 1;
+  }
+}
 
- 
-let mostEdges = [...Object.keys(bridgeMap)].sort((a,b)=> bridgeMap[b]-bridgeMap[a]).map((y)=> [y,bridgeMap[y]]).slice(0,3)
-console.log(mostEdges)
+// Top 3 edges
+let topEdges = [...Object.keys(bridgeMap)].sort((a,b)=> bridgeMap[b]-bridgeMap[a]).flatMap((y)=> [y.split('_')]).slice(0,3)
 
-let [e1,e2] = mostEdges[0][0].split('_')
-let [e3,e4] = mostEdges[1][0].split('_')
-let [e5,e6] = mostEdges[2][0].split('_')
-console.log(e1,e2,e3,e4,e5,e6)
+let [[e1,e2],[e3,e4],[e5,e6]] = topEdges
 
-
-wiring.set(e1,wiring.get(e1).filter((x)=> x!==e2))
-wiring.set(e2,wiring.get(e2).filter((x)=> x!==e1))
-wiring.set(e3,wiring.get(e3).filter((x)=> x!==e4))
-wiring.set(e4,wiring.get(e4).filter((x)=> x!==e3))
-wiring.set(e5,wiring.get(e5).filter((x)=> x!==e6))
-wiring.set(e6,wiring.get(e6).filter((x)=> x!==e5))
+topEdges.forEach(([n1,n2])=>{
+  wiring.set(n1,wiring.get(n1).filter((x)=> x!==n2));
+  wiring.set(n2,wiring.get(n2).filter((x)=> x!==n1));
+})
 
 
-console.log(wiring.get(e1))
-console.log(wiring.get(e2))
-console.log(wiring.get(e3))
-console.log(wiring.get(e4))
-console.log(wiring.get(e5))
-console.log(wiring.get(e6))
-
+// Count all connected nodes in each group
 let p1seen = new Set ()
 let p1queue = [e1]
 
 while(p1queue.length>0){
-  let queueitem = p1queue.shift()
-  p1seen.add(queueitem)
-  let nextitems = wiring.get(queueitem)
+  let queueitem = p1queue.shift();
+  p1seen.add(queueitem);
+  let nextitems = wiring.get(queueitem);
   if(nextitems !== undefined && nextitems.length>0){
     nextitems.forEach((nitem)=>{
       if(!p1seen.has(nitem)){
-        p1queue.push(nitem)
+        p1queue.push(nitem);
       }
     })
   }
@@ -150,19 +111,19 @@ let p2seen = new Set ()
 let p2queue = [e2]
 
 while(p2queue.length>0){
-  let queueitem = p2queue.shift()
-  p2seen.add(queueitem)
-  let nextitems = wiring.get(queueitem)
+  let queueitem = p2queue.shift();
+  p2seen.add(queueitem);
+  let nextitems = wiring.get(queueitem);
   if(nextitems !== undefined && nextitems.length>0){
     nextitems.forEach((nitem)=>{
       if(!p2seen.has(nitem)){
-        p2queue.push(nitem)
+        p2queue.push(nitem);
       }
     })
   }
 }
 
-console.log(p1seen.size)
-console.log(p2seen.size)
+console.log(p1seen.size);
+console.log(p2seen.size);
 console.log(p1seen.size*p2seen.size) // Part 1 answer
 
