65d8bab730cd24a8780e695e179e3827dde37d94
Cory-Ann Joseph <coryannj@gmail.com>
2024-06-14 17:28:10 +1000

    Day 7 cleanup

--- Day 07.js
+++ Day 07.js
@@ -3,127 +3,57 @@ const { default: test } = require('node:test');
 const { mainModule } = require('process');
 const input = fs.readFileSync('../day7input.txt',{ encoding: 'utf8', flag: 'r' });
 
-const lineSplit = /(?<=^[$]\s)([.\w|\s]*)/gm
+let lines = input.replaceAll('$ ','').replace('\/','root').split(/[\r\n]+/).filter((x)=> x !== 'ls').map((y)=> y.split(' '))
 
-let output = input.replace('\/','|').match(lineSplit)
-//console.log(output)
+// Part 1
+let fileMap = {}
+let [firstc,firstv] = lines.shift();
 
+fileMap[`${firstv}_0`]={allParents:[],dir:[],filenames:[],size:0};
+let currentDir = `${firstv}_0`;
+let currentLevel = 0;
 
-let first = output.shift().match(/\S+/g)
-filemap = {}
-filemap['root_0']={dir:[first[1]],level:0}
-filemap[`root_0_${first[1]}_1`]={parentDir:'root_0',level:1}
-console.log(filemap)
-let currentDir = `root_0_${first[1]}_1`
-let currentLevel = 1
-
-output.forEach((line)=>{
-    console.log('line is ',line)
-    if(line.charAt(0) === 'l') {
-        let split = line.trim().split(/[\r\n]+/).slice(1).map((y)=> y.match(/\S+/g))
-        //console.log(split)
-        split.forEach(([f1,f2])=>{
-            //console.log('f1,f2 is ',f1,f2)
-            if (f1 === 'dir') {
-               // console.log('f1 is dir')
-                if (filemap[currentDir][f1] === undefined){
-                    filemap[currentDir][f1] = []
-                }
-                filemap[currentDir][f1].push(`${currentDir}_${f2}_${currentLevel+1}`)
-                
-                if (filemap[`${currentDir}_${f2}_${currentLevel+1}`] === undefined){
-                    filemap[`${currentDir}_${f2}_${currentLevel+1}`] = {}
-                    filemap[`${currentDir}_${f2}_${currentLevel+1}`]['parentDir']=currentDir
-                    filemap[`${currentDir}_${f2}_${currentLevel+1}`]['level']=currentLevel+1
-                }
-            } else {
-                //console.log('f1 is something else')
-                if (filemap[currentDir]['filesizes']=== undefined){
-                    filemap[currentDir]['filesizes']=[]
-                }
-                if (filemap[currentDir]['filenames']=== undefined){
-                    filemap[currentDir]['filenames']=[]
-                }
-                
-                if (!filemap[currentDir]['filenames'].includes(`${f1}_${f2}`)){
-                    filemap[currentDir]['filenames'].push(`${f1}_${f2}`)
-                    filemap[currentDir]['filesizes'].push(parseInt(f1))
-                }
-            }
-            
-        })
-        //console.log(filemap)
+// Populate directory - key is full path and level to avoid duplicate key issues e.g. 'root_0_vvsg_1_bwv_2_gwdh_3'
+lines.forEach(([command,val])=>{
+    if (command === 'cd'){
+        if (val === '..') {
+            currentLevel--;
+            currentDir = fileMap[currentDir]['parentDir'];
+        } else {
+            currentLevel++;
+            currentDir = fileMap[currentDir]['dir'].find((x)=>x.includes(`${val}_${currentLevel}`));
+        }
     } else {
-        let [command,val] = line.match(/\S+/g)
-        if (command === 'cd') {
-            if (val !== '..') {
-                currentLevel++
-                let nextCurrentDir = filemap[currentDir]['dir'].find((x)=>x.includes(`${val}_${currentLevel}`))
+        if (command === 'dir') {
+            fileMap[currentDir]['dir'].push(`${currentDir}_${val}_${currentLevel+1}`);
 
-                currentDir = nextCurrentDir
-                
-                
-                console.log('currentDir is now ',currentDir,' current level is now ',currentLevel)
-            } else {
-                let parent = filemap[currentDir]['parentDir']
-                currentDir = parent
-                currentLevel--
-               // console.log('currentDir is now ',currentDir,' current level is now ',currentLevel)
-            } 
+            if (!fileMap[`${currentDir}_${val}_${currentLevel+1}`]){
+                fileMap[`${currentDir}_${val}_${currentLevel+1}`] = {
+                    'parentDir':currentDir,
+                    'allParents': fileMap[currentDir]['allParents'].concat(currentDir),
+                    'dir':[],
+                    'filenames':[],
+                    'size':0
+                };
+            }
+        } else {
+            if (!fileMap[currentDir]['filenames'].includes(`${command}_${val}`)){
+                fileMap[currentDir]['filenames'].push(`${command}_${val}`);
+                fileMap[currentDir]['size']+=parseInt(command);
+                fileMap[currentDir]['allParents'].forEach((parent)=>fileMap[parent]['size']+=parseInt(command)); // Add file size to all parents
+            };
         }
     }
-
-    
 })
-console.log(filemap)
-
-//console.log(Object.entries(filemap))
-
-let fileSizeMap = {}
-Object.keys(filemap).forEach((key)=>fileSizeMap[key]=0)
-
-let allLevels = Object.values(filemap).map((x)=>x.level).sort((a,b)=>b-a)
-let levels = [...new Set(allLevels)]
-console.log(levels)
-
-levels.forEach((thisLevel)=>{
-    let levelQueue = [...Object.entries(filemap)].filter(([key,value])=>value.level === thisLevel)
-
-    console.log(levelQueue.map(([key,val])=>val.filesizes !== undefined ? eval(val.filesizes.join('+')):0).reduce((acc,curr)=>acc+curr))
-    //console.log('levelQueue is ',levelQueue)
-    levelQueue.forEach(([nkey,nvalue]) => {
-        // Update filesize for current directory
-        if(nvalue.filesizes !== undefined && nvalue.filesizes.length>0){
-            let fileSize = nvalue.filesizes.reduce((acc,curr)=> acc+curr)
-            fileSizeMap[nkey]+=fileSize
-        }
-
-        // Add filesize to parent directory and add parent to queue
-        if (nvalue.parentDir !== undefined){
-            fileSizeMap[nvalue.parentDir]+=fileSizeMap[nkey]
-        }
-    })
-    console.log([...Object.entries(fileSizeMap)].filter(([key,value])=>parseInt(key.slice(-1))===thisLevel).map((x)=>x[1]).reduce((acc,curr)=>acc+curr))
-})
-
-console.log(fileSizeMap)
 
 let limit = 100000
-
-let underLimit = [...Object.values(fileSizeMap)].filter((x)=> x<=limit)
-console.log(underLimit)
-//console.log(underLimit.map((x)=>))
-
-
-
-
-console.log(underLimit.reduce((acc,curr)=>acc+curr)) // Part 1 answer
+let underLimit = [...Object.values(fileMap)].map((val)=>val.size).filter((x)=> x<=limit).reduce((acc,curr)=>acc+curr);
+console.log(underLimit) // Part 1 answer
 
 //Part 2
-let totalSpace = 70000000
-let requiredSpace = 30000000
-let usedSpace = fileSizeMap['root_0']
-let spaceNeeded = requiredSpace-(totalSpace-usedSpace)
-
-let findDirectory = Object.values(fileSizeMap).filter((x)=> x>=spaceNeeded).sort((a,b)=>a-b)
-console.log(findDirectory[0]) // Part 2 answer
+let totalSpace = 70000000;
+let requiredSpace = 30000000;
+let usedSpace = fileMap[`${firstv}_0`]['size'];
+let spaceNeeded = requiredSpace-(totalSpace-usedSpace);
+let findDirectory = Object.values(fileMap).map((obj)=>obj.size).filter((x)=> x>=spaceNeeded);
+console.log(Math.min(...findDirectory)) // Part 2 answer
