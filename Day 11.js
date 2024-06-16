b252f6c9b47c48a2d4647fb825124ea3e6c767fe
Cory-Ann Joseph <coryannj@gmail.com>
2024-06-16 15:15:27 +1000

    Day 11

--- Day 11.js
+++ Day 11.js
@@ -6,105 +6,66 @@ const testRegex = /(?<=[:]\s)(.*)/
 
 let lines = input.split(/\n\n/).map((x)=> x.split(/[\r\n]+/).map((y,idx)=>{
     if (idx === 1) {
-        return y.match(/(\d+)/g).map(Number)
+        return y.match(/(\d+)/g).map(Number) // items array
     } else if (idx === 2) {
-        return y.match(opRegex)[0].split(' ')
-    } else if (idx === 3) {
-        return y.match(/(\d+)/g).map(Number)[0]
-    } else if (idx > 3 && idx <= 5) {
-        return parseInt(y.slice(-1))
+        return y.match(opRegex)[0] // operations
     } else {
-        return y.match(/(\d+)/g).map(Number)[0]
+        return y.match(/(\d+)/g).map(Number)[0] // Monkey number, test, true, false
     }
 }))
-//console.log(lines)
 
 let monkeys = lines.map((x)=> [x[0],x[1]])
-let monkeyMap = new Map()
-let monkeyQueue = new Map()
-//console.log(monkeys)
+let p1monkeyMap = new Map()
+let p1monkeyQueue = new Map()
+let p2monkeyMap = new Map()
+let p2monkeyQueue = new Map()
+let lcm = lines.map((x)=>x[3]).reduce((acc,curr)=> acc*curr,1)
 
 monkeys.forEach(([key,items])=>{
-    monkeyMap.set(key,[])
-    monkeyQueue.set(key,items)
+    p1monkeyMap.set(key,[])
+    p1monkeyQueue.set(key,items.slice(0))
+    p2monkeyMap.set(key,[])
+    p2monkeyQueue.set(key,items.slice(0))
 })
 
-let rounds = 20
+const getQueue = (p1orp2,monkeyidx) => p1orp2 === 'p1' ? p1monkeyQueue.get(monkeyidx) : p2monkeyQueue.get(monkeyidx)
+const updateQueue = (p1orp2,monkeyidx,worry) => p1orp2 === 'p1' ? p1monkeyQueue.get(monkeyidx).push(worry) : p2monkeyQueue.get(monkeyidx).push(worry)
 
-for(i=0;i<rounds;i++) {
-    let monkeyRound = [...monkeyQueue.keys()]
-    console.log('i is ',i,' and monkeyRound is ',monkeyRound)
-
-    monkeyRound.forEach((monkey)=>{
-        let mqueue = monkeyQueue.get(monkey)
-        console.log('monkey is ',monkey,' and mqueue is ',mqueue)
-        let [monkeyKey,itemsignore,operation,test,iftrue,iffalse] = lines.find((x)=>x[0]===monkey)
-        //console.log(monkeyKey,itemsignore,operation,test,iftrue,iffalse)
-
-        while (mqueue.length>0) {
-            item = mqueue.shift()
-            monkeyMap.get(monkey).push(item)
-            let operationNum = operation[2] === 'old' ? item : parseInt(operation[2])
-            //console.log('item is ',item,' operationNum is ',operationNum)
-            let operationMap = {
-                '+': item+operationNum,
-                '-': item-operationNum,
-                '*': item*operationNum,
-                '/': item/operationNum
-            }
-            //console.log('operationMap is ',operationMap[operation[1]])
+const monkeyBusiness = (noOfRounds,p1orp2) => {
+    for(i=0;i<noOfRounds;i++) {
+        let monkeyRound = [...p1monkeyQueue.keys()]
     
-            //worry = operationMap[operation[1]]
-            worry = Math.floor(operationMap[operation[1]]/3)
+        monkeyRound.forEach((monkey)=>{
+            let [monkeyKey,itemsignore,operation,test,iftrue,iffalse] = lines.find((x)=>x[0]===monkey)
     
-            if (worry%test === 0) {
-                monkeyQueue.get(iftrue).push(worry)
-            } else {
-                monkeyQueue.get(iffalse).push(worry)
-            }
+            while (getQueue(p1orp2,monkey).length>0) {
+                let old = getQueue(p1orp2,monkey).shift();
+                let worry
 
-        }
-    })
-    console.log(monkeyQueue)
+                if (p1orp2 === 'p1') {
+                    worry = Math.floor(eval(operation)/3)
+                    p1monkeyMap.get(monkey).push(old)
+                } else {
+                    worry = Math.floor(eval(operation)) > lcm ? Math.floor(eval(operation))%lcm : Math.floor(eval(operation))
+                    p2monkeyMap.get(monkey).push(old)
+                }
+
+                if (worry%test === 0) {
+                    updateQueue(p1orp2,iftrue,worry)
+                } else {
+                    updateQueue(p1orp2,iffalse,worry)
+                }
+    
+            }
+    
+        })
+    }
+    return p1orp2 === 'p1' ? [...p1monkeyMap.values()].map((x,ix)=>x.length) : [...p2monkeyMap.values()].map((x,ix)=>x.length)
 }
 
-console.log(monkeyQueue)
-console.log([...monkeyMap.values()].map((x,ix)=>x.length))
-console.log([...monkeyMap.values()].map((x,ix)=>x.length).sort((a,b)=> b-a).slice(0,2).reduce((acc,curr)=>acc*curr,1))
+let p1rounds = 20
+let p2rounds = 10000
 
-
-// while(rounds<20){
-//     rounds++
-//     [monkeyKey,items,operation,test,iftrue,iffalse] = queue.shift()
-//     console.log(monkeyKey,items,operation,test,iftrue,iffalse)
-
-//     items.forEach((item)=>{
-//         monkeyMap.get(monkeyKey).push(item)
-//         let operationNum = operation[2] === 'old' ? item : parseInt(operation[2])
-
-//         let operationMap = {
-//             '+': item+operationNum,
-//             '-': item-operationNum,
-//             '*': item*operationNum,
-//             '/': item/operationNum
-//         }
-//         console.log('operationMap is ',operationMap[operation[1]])
-
-//         worry = Math.floor(operationMap[operation[1]]/3)
-
-//         if (worry%test === 0) {
-//             let nextMonkeyTrue = lines.find((x)=> x[0] === iftrue)
-//             nextMonkeyTrue[1] = [worry]
-//             console.log('nextmonkeytrue ',nextMonkeyTrue)
-//             queue.push(nextMonkeyTrue)
-//         } else {
-//             let nextMonkeyFalse = lines.find((x)=> x[0] === iffalse)
-//             nextMonkeyFalse[1] = [worry]
-//             console.log('nextMonkeyFalse',nextMonkeyFalse)
-//             queue.push(nextMonkeyFalse)
-//         }
-//     })
-// }
-
-// console.log(monkeyMap)
+console.log(monkeyBusiness(p1rounds,'p1').sort((a,b)=> b-a).slice(0,2).reduce((acc,curr)=> acc*curr,1)) // Part 1 answer
+console.log(monkeyBusiness(p2rounds,'p2').sort((a,b)=> b-a).slice(0,2).reduce((acc,curr)=> acc*curr,1)) // Part 2 answer
 
