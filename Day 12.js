a88e59dcea0c0e7b89d062779cb025d964c7124f
Cory-Ann Joseph <coryannj@gmail.com>
2024-08-20 22:21:38 +1000

    Day 12 cleanup

--- Day 12.js
+++ Day 12.js
@@ -8,58 +8,29 @@ let lines = input.split(/[\r\n]+/).map((x)=>[x.charAt(0),Number(x.slice(1))])
 function move(currpos,action){
     let [r,c,dir] = currpos
     let [nd,steps] = action
+
     let nextSteps = {
-        'N':{
-            'N':[r-steps,c,'N'],
-            'S':[r+steps,c,'N'],
-            'E':[r,c+steps,'N'],
-            'W':[r,c-steps,'N'],
-            'F':[r-steps,c,'N']
-        },
-        'S':{
-            'N':[r-steps,c,'S'],
-            'S':[r+steps,c,'S'],
-            'E':[r,c+steps,'S'],
-            'W':[r,c-steps,'S'],
-            'F':[r+steps,c,'S']
-        },
-        'E':{
-            'N':[r-steps,c,'E'],
-            'S':[r+steps,c,'E'],
-            'E':[r,c+steps,'E'],
-            'W':[r,c-steps,'E'],
-            'F':[r,c+steps,'E']
-        },
-        'W':{
-            'N':[r-steps,c,'W'],
-            'S':[r+steps,c,'W'],
-            'E':[r,c+steps,'W'],
-            'W':[r,c-steps,'W'],
-            'F':[r,c-steps,'W']
-        }
+        'N':[r+steps,c,dir],
+        'S':[r-steps,c,dir],
+        'E':[r,c+steps,dir],
+        'W':[r,c-steps,dir]
     }
-
-    let nextFacing = {
-        'NR': {90: 'E',180:'S', 270: 'W'},
-        'NL': {90: 'W',180:'S', 270: 'E'},
-        'SR': {90: 'W',180:'N', 270: 'E'},
-        'SL': {90: 'E',180:'N', 270: 'W'},
-        'ER': {90: 'S',180:'W', 270: 'N'},
-        'EL': {90: 'N',180:'W', 270: 'S'},
-        'WR': {90: 'N',180:'E', 270: 'S'},
-        'WL': {90: 'S',180:'E', 270: 'N'},
-    }
-
-    if('NSEWF'.includes(nd)){
-        return nextSteps[dir][nd]
+    
+    if(nd === 'F'){
+        return nextSteps[dir]
+    } else if ('NSEW'.includes(nd)){
+        return nextSteps[nd]
     } else {
-        return [r,c,nextFacing[`${dir}${nd}`][steps]]
+        let nextFacing = ['N','E','S','W'];
+        let currIndex = nextFacing.indexOf(dir);
+        let degrees = nd === 'L' ? -1*(steps/90) : (steps/90);
+    
+        return [r,c,nextFacing.at((currIndex+degrees)%4)]
     }
 }
 
-let p1Pos = [0,0,'E']
-
-let p1Actions = structuredClone(lines)
+let p1Pos = [0,0,'E'];
+let p1Actions = structuredClone(lines);
 
 while(p1Actions.length>0){
     let nextpos = move(p1Pos,p1Actions.shift())
@@ -68,9 +39,7 @@ while(p1Actions.length>0){
 
 console.log(Math.abs(0-p1Pos[0])+Math.abs(0-p1Pos[1])) // Part 1 answer
 
-
-
-
+// Part 2
 function p2Move(currpos,waypoint,action){
     let [r,c,dir] = currpos
     let [nd,steps] = action
@@ -82,7 +51,7 @@ function p2Move(currpos,waypoint,action){
             'L':[[wc,-wr],[-wr,-wc],[-wc,wr]]
         }
 
-        let degreesIndex = (steps/90) -1
+        let degreesIndex = (steps/90)-1
 
         return{'position':currpos,'waypoint':rotation[nd][degreesIndex]}
 
