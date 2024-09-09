6bfa1bedf4b97524330f3aaf9648acb3ba4719aa
Cory-Ann Joseph <coryannj@gmail.com>
2024-09-10 00:24:12 +1000

    Day 22 part 2

--- Day 22.js
+++ Day 22.js
@@ -1,230 +1,173 @@
 const fs = require('fs');
+const { on } = require('stream');
 const input = fs.readFileSync('../day22input.txt',{ encoding: 'utf8', flag: 'r' });
 
 const splitregex = /(on|off|[-]{0,1}\d+)/g
-const lines = input.split(/[\r\n]+/).map((x)=>x.match(splitregex).map((y,yidx)=>yidx>0?parseInt(y):y)).map((z)=>{
-    return [z[0],{'x':[z[1],z[2]],'y':[z[3],z[4]],'z':[z[5],z[6]]}]
-})
+const lines = input.split(/[\r\n]+/).map((x)=>x.match(splitregex).map((y,yidx)=>yidx>0?parseInt(y):y)).map((z,zx)=>{return[zx,z[0],z.slice(1)]})
 
-//console.log(lines)
+
+// From https://stackoverflow.com/questions/12303989/cartesian-product-of-multiple-arrays-in-javascript
+function* cartesian(head, ...tail) {
+    const remainder = tail.length > 0 ? cartesian(...tail) : [[]];
+    for (let r of remainder) for (let h of head) yield [h, ...r];
+}
+
+function getVolume(coOrds){
+    let [x1,x2,y1,y2,z1,z2] = coOrds
+    return (Math.abs((x2 - x1))+1)*(Math.abs((y2 - y1))+1)*(Math.abs((z2 - z1))+1)
+}
 
 function testOverlap(range1,range2){
-    let [x1,x2,y1,y2,z1,z2] = Object.values(range1).flat()
-    let [bx1,bx2,by1,by2,bz1,bz2] = Object.values(range2).flat()
-    //console.log('range1test',[x1,x2,y1,y2,z1,z2])
-    //console.log('range2test',[bx1,bx2,by1,by2,bz1,bz2])
-
-    return [Math.max(x1,bx1) <= Math.min(x2,bx2),Math.max(y1,by1) <= Math.min(y2,by2),Math.max(z1,bz1) <= Math.min(z2,bz2)]
-
+    let [x1,x2,y1,y2,z1,z2] = range1
+    let [bx1,bx2,by1,by2,bz1,bz2] = range2
+    return Math.max(x1,bx1) <= Math.min(x2,bx2) && Math.max(y1,by1) <= Math.min(y2,by2) && Math.max(z1,bz1) <= Math.min(z2,bz2)
 }
 
-let p2lines = lines.slice().filter(([a,r])=>Object.values(r).flat().every((x)=> x<-50 || x>50)).map(([a,r])=>r).sort((a,b)=>a['z'][0]-b['z'][0])
-p2lines.forEach((x)=>console.log(x))
-
-let one = p2lines[0]
-let rest = p2lines.slice(1)
-rest.forEach(([a,r],idx)=>console.log('action is ',a,' idx is ',idx,' overlap ',testOverlap(one[1],r)))
-
-function on(range1,range2){
-    let overlap = testOverlap(range1,range2)
-    let r1 = Object.values(range1)
-    let r2 = Object.values(range2)
-
-    let [x1,x2,y1,y2,z1,z2] = r1.flat()
-    let [bx1,bx2,by1,by2,bz1,bz2] = r2.flat()
-    if(overlap.every((x)=>x === false)){
-        return [range1,range2]
-    } else if (overlap.every((x)=> x === true)){
-        return [{
-            'x':[Math.min(x1,bx1),Math.max(x2,bx2)],
-            'y':[Math.min(y1,by1),Math.max(y2,by2)],
-            'z':[Math.min(z1,bz2),Math.max(z1,bz2)]
-        }]
+function testSuperset(cubes1,cubes2){
+    let [x1,x2,y1,y2,z1,z2] = cubes1
+    let [bx1,bx2,by1,by2,bz1,bz2] = cubes2
+    if(testOverlap(cubes1,cubes2)){
+        return [[x1<=bx1 && bx2 <=x2,y1<=by1 && by2 <=y2,z1<=bz1 && bz2 <=z2].every((x)=>x === true),[bx1<=x1 && x2 <=bx2,by1<=y1 && y2 <=by2,bz1<=z1 && z2 <=bz2].every((x)=> x === true)]
     } else {
-        console.log('only some overlap')
-        let ranges = overlap.map((x,idx)=>{            
-            if(x === false){
-                return[r1[idx],r2[idx]]
-            } else {
-                return[Math.min(r1[idx][0],r2[idx][0]),Math.max(r1[idx][1],r2[idx][1])]
+        return [false,false]
+    }
+}
+
+function getOverlap(cubes1,cubes2){
+    let [x1,x2,y1,y2,z1,z2] = cubes1
+    let [bx1,bx2,by1,by2,bz1,bz2] = cubes2
+
+    if(testOverlap(cubes1,cubes2)){
+        let overlapping = [Math.max(x1,bx1),Math.min(x2,bx2),Math.max(y1,by1),Math.min(y2,by2),Math.max(z1,bz1),Math.min(z2,bz2)]
+
+        return [testSuperset([x1,x2,y1,y2,z1,z2],[bx1,bx2,by1,by2,bz1,bz2]),
+                [getVolume(overlapping)],
+                [overlapping]].flatMap((x)=>x)
+        
+    } else {
+        return []
+    }
+}
+
+function intervals(start,end){
+    let s = start.slice()
+    let e = end.slice()
+    let i1 = s.shift()
+    let result = []
+    while(s.length>0||e.length>0){
+        let nextStart = s[0]
+        let nextEnd = e[0]
+
+        if(nextStart<=nextEnd || nextEnd === undefined){
+            result.push([i1,nextStart-1])
+            i1 = s.shift()
+        } else {
+            result.push([i1,e.shift()])
+            i1 = nextEnd+1
+        }
+    }
+    return result
+}
+
+// Part 1
+let p1steps = {}
+
+lines.filter(([ix,el,vals])=>vals.every((x)=>-50<=x && x<=50)).forEach((line)=>{
+    let [ind,action,[x1,x2,y1,y2,z1,z2]] = line
+
+    for(i=x1;i<=x2;i++){
+        for(j=y1;j<=y2;j++){
+            for(k=z1;k<=z2;k++){
+                if(action === 'on'){
+                    p1steps[`${i}_${j}_${k}`] = 'on'
+                } else {
+                    p1steps[`${i}_${j}_${k}`] = 'off'
+                }
             }
-        })
-        return ranges
+        }
     }
+})
 
-
-    // all x,y,z ranges don't overlap
-        // return array with both range objects
-    // all x,y,z ranges overlap
-        // return array with min/max of both
-    // some x,y,z overlaps, and some x,y,z doesn't
-}
-
-let t1 = { x:[-49,1],y:[-3,46],z:[-24,28]}
-let t2 = { x:[2,47],y:[-22,22],z:[-23,27]}
-
-console.log(on(t1,t2))
-
-
-let first = on(lines[0][1],lines[1][1])
-let second = on(first[0],lines[2][1])
-let third = on(second[0],lines[3][1])
-let fourth = on(third[0],lines[4][1])
-
-
-let fifth = on(fourth[0],lines[5][1])
-console.log(testOverlap(fourth[0],lines[5][1]))
-console.log(fifth)
-
-let area = lines.filter((x,ix,arr)=> x.slice(1).every((y)=> -50<=y && y<=50))
-//console.log(area)
-
-let parse = area.slice()
-
-let parLen = parse.length
-
-let offIndex = parse.findIndex((x)=>x[0]==='off')
-console.log(offIndex)
-
-let slice = parse.slice(0,offIndex)
-let range = [
-    Math.min(...slice.map((x)=>x[1])),
-    Math.max(...slice.map((x)=>x[2])),
-    Math.min(...slice.map((x)=>x[3])),
-    Math.max(...slice.map((x)=>x[4])),
-    Math.min(...slice.map((x)=>x[5])),
-    Math.max(...slice.map((x)=>x[6]))
-]
-
-console.log(range)
-
-console.log(parse.at(offIndex))
-
-for(i=0;i<parLen-1;i++){
-    let [a,x1,x2,y1,y2,z1,z2] = parse[i]
-    let [b,bx1,bx2,by1,by2,bz1,bz2] = parse[i+1]
-    console.log('**** I is ',i,' ****')
-    console.log('x overlap',Math.max(x1,bx1) <= Math.min(x2,bx2))
-    console.log('y overlap',Math.max(y1,by1) <= Math.min(y2,by2))
-    console.log('z overlap',Math.max(z1,bz1) <= Math.min(z2,bz2))
-}
-
-
-let curr = parse.shift()
-console.log(curr)
-let queue = parse
-
-while(queue.length>0){
-
-}
-
-
-let pos = 0
-let p1pos = lines[0]
-let p2pos = lines[1]
-let turn = 0
-let p1score = 0
-let p2score = 0
-
-while (p1score < 1000 && p2score < 1000){
-    let score = [(pos%100)+1,((pos+1)%100)+1,((pos+2)%100)+1]
-    let scoreIndex = score.reduce((acc,curr)=>acc+curr,0)
-    if(turn%2 === 0){ // p1
-        let newscore = (p1pos+scoreIndex-1)%10+1
-        p1score+=newscore
-        //console.log('Player 1 rolls ',score,' and moves to space ',newscore,'  for a total score of ',p1score)
-        p1pos = newscore
-    } else { // p2
-        let newscore = (p2pos+scoreIndex-1)%10+1
-        p2score+=newscore
-        //console.log('Player 1 rolls ',score,' and moves to space ',newscore,'  for a total score of ',p2score)
-        p2pos = newscore
-    }
-    pos+=3
-    turn++
-}
-
-console.log(Math.min(p1score,p2score)*turn*3) // Part 1 answer
+let p1Total = Object.values(p1steps).filter((x)=>x === 'on').length 
+console.log(p1Total) // Part 1 answer
 
 // Part 2
 
-// Calculate frequencies for dice rolls per turn
-function* permutate(items, count) {
-    yield* req([])
-  
-    function* req(array) {
-      if (array.length == count) {
-        yield array.map(Number)
-        return
-      }
-      for (const item of items) {
-        yield* req(array.concat(item))
-      }
-    }
-  }
-  
-let universes = [...permutate(['1', '2', '3'], 3)].map((x)=>x.reduce((acc,curr)=>acc+curr,0)).sort((a,b)=>a-b).reduce((m, k) => { m[k] = m[k] + 1 || 1; return m }, {})
-let universeMap = new Map(Object.entries(universes).map(([x,y])=>[parseInt(x),y]))
+let p2lines = lines.filter(([ix,k,v])=>v.every((y)=>y<-50||y>50)) 
+let p2OnlyOn = p2lines.filter((x)=>x[1]==='on')
 
-function getUniverses(startArr){
-    let queue  = [[startArr]]
-    let result = []
-    let under = []
-    while (queue.length>0){
-        let thisVal = queue.shift()
-        let [lastScore,lastDice,lastPos] = thisVal.at(-1)
-        
-        for(i=3;i<=9;i++){
-            let nextVal = ((lastPos+i-1)%10)+1
-            if(lastScore+nextVal>=21){
-                result.push(thisVal.concat([[lastScore+nextVal,i,nextVal]]))
-            } else {
-                queue.push(thisVal.concat([[lastScore+nextVal,i,nextVal]]))
-                under.push(thisVal.concat([[lastScore+nextVal,i,nextVal]]))
-            }
+// Map all future intersections for all 'on' cubes + co-ordinates
+let allOn = p2OnlyOn.map((x)=>{
+    let futureOverlaps = lines.filter((y)=>y[0]>x[0])
+        if (futureOverlaps.length>0){
+           return [x,futureOverlaps.map(([ind,action,vals])=>[ind,action,vals,getOverlap(x[2],vals)]).filter((y)=>y[3].length>0).map(([ind,action,vals,overlaps])=>[ind,action,overlaps[3]])]
+        } else {
+            return [x,[]]
+        }
+})
+
+// Volume of cubes with zero intersections afterwards
+let alwaysOn = allOn.filter((x)=>x[1].length === 0).map((x)=>getVolume(x[0][2])).reduce((acc,curr)=>acc+curr)
+
+// Volume of remaining cubes
+let overlapsOn = allOn.filter((x)=>x[1].length>0)
+let p2Total = 0
+
+overlapsOn.forEach(([first,overlapsFirst])=>{
+    let overlapsFirstRemove = overlapsFirst.filter(([ind,action,vals],ix,arr)=> arr.findIndex(([find,faction,fvals])=> (find>ind && testSuperset(vals,fvals)[1]===true && testSuperset(vals,fvals)[0]===false)|| (find<ind && testSuperset(vals,fvals)[1]===true && testSuperset(vals,fvals)[0]===false))===-1)
+    let rest = overlapsFirstRemove.filter(([ind,action,vals],ix,arr)=> arr.findIndex(([find,faction,fvals])=> find>ind && testSuperset(vals,fvals)[1]===true)===-1)
+
+    let firstVolume = getVolume(first[2])
+    let firstRest = rest.map((x)=>x[2])
+    let restVolume = firstRest.map((x)=>getVolume(x)).sort((a,b) => {
+        if(a > b) {
+          return 1;
+        } else if (a < b){
+          return -1;
+        } else {
+          return 0;
+        }
+      })
+    
+    let overlapsVolume = 0
+
+    if(rest.length === 1){
+        overlapsVolume+=getVolume(rest[0][2])
+    }else {
+        let fxMin = [...new Set(firstRest.map((x)=>x[0]))].sort((a,b)=>a-b)
+        let fxMax = [...new Set(firstRest.map((x)=>x[1]))].sort((a,b)=>a-b)
+        let fyMin = [...new Set(firstRest.map((x)=>x[2]))].sort((a,b)=>a-b)
+        let fyMax = [...new Set(firstRest.map((x)=>x[3]))].sort((a,b)=>a-b)
+        let fzMin = [...new Set(firstRest.map((x)=>x[4]))].sort((a,b)=>a-b)
+        let fzMax = [...new Set(firstRest.map((x)=>x[5]))].sort((a,b)=>a-b)
+    
+        let xIntervals = intervals(fxMin,fxMax)
+        let yIntervals = intervals(fyMin,fyMax)
+        let zIntervals = intervals(fzMin,fzMax)
+    
+        const sweep = cartesian(xIntervals,yIntervals,zIntervals) // slicing all intersections
+
+        let coOrds = sweep.next().value.flat()
+
+        while(coOrds !== undefined){
+
+            let overlapsTest = rest.map(([ind,action,vals])=>[ind,action,vals,getOverlap(vals,coOrds)]).filter((y)=>y[3].length>0)
             
+            let overlapsRemove1 = overlapsTest.filter(([ind,action,vals,overs],ix,arr)=> arr.findIndex(([find,faction,fvals,fovers])=> (find>ind && testSuperset(overs[3],fovers[3])[1]===true &&testSuperset(overs[3],fovers[3])[0]===false) || (find<ind && testSuperset(overs[3],fovers[3])[1]===true &&testSuperset(overs[3],fovers[3])[0]===false))===-1)
+            let overlaps = overlapsRemove1.filter(([ind,action,vals,overs],ix,arr)=> arr.findIndex(([find,faction,fvals,fovers])=> find>ind && testSuperset(overs[3],fovers[3])[1]===true)===-1) // filters out subsets
+    
+            if(overlaps.length>0){
+                    overlapsVolume += overlaps[0][3][2] // Not generalised - in my input there was never more than one overlap returned
+            }
+
+            let nextCoord = sweep.next().value
+            coOrds = nextCoord === undefined ? nextCoord : nextCoord.flat()
         }
     }
-    return {'result':result,'under':under}
-}
 
-let p1 = getUniverses([0,0,p1pos])
-let p1over = p1['result']
-let p1under = p1['under']
-let p1OverTurns = [...new Set(p1over.map((x)=>x.slice(1).length))]
-let p1OverUniverses = p1over.map((x)=> x.map((y)=>y[1]).slice(1).map((z)=>universeMap.get(z)))
-let p1UnderUniverses = p1under.map((x)=> x.map((y)=>y[1]).slice(1).map((z)=>universeMap.get(z)))
+    p2Total+=(firstVolume-overlapsVolume)
 
-let p2 = getUniverses([0,0,p2pos])
-let p2over = p2['result']
-let p2under = p2['under']
-let p2OverTurns = [...new Set(p2over.map((x)=>x.slice(1).length))]
-let p2OverUniverses = p2over.map((x)=> x.map((y)=>y[1]).slice(1).map((z)=>universeMap.get(z)))
-let p2UnderUniverses = p2under.map((x)=> x.map((y)=>y[1]).slice(1).map((z)=>universeMap.get(z)))
-
-let p1Wins = 0
-let p2Wins = 0
-
-p1OverTurns.forEach((len)=>{
-    let p1Win = p1OverUniverses.filter((x)=>x.length === len).flatMap((y)=>y.reduce((a,b)=>a*b,1))
-    let p2Lose = p2UnderUniverses.filter((x)=>x.length === len-1).flatMap((y)=>y.reduce((a,b)=>a*b,1))
-
-    p1Win.forEach((p1Total)=>{
-        p2Lose.forEach((p2Total)=>{
-            p1Wins+=(p1Total*p2Total)
-        })
-    })
 })
 
-p2OverTurns.forEach((len)=>{
-    let p2Win = p2OverUniverses.filter((x)=>x.length === len).flatMap((y)=>y.reduce((a,b)=>a*b,1))
-    let p1Lose = p1UnderUniverses.filter((x)=>x.length === len).flatMap((y)=>y.reduce((a,b)=>a*b,1))
+console.log(p1Total+alwaysOn+p2Total)
 
-    p2Win.forEach((p2Total)=>{
-        p1Lose.forEach((p1Total)=>{
-            p2Wins+=(p2Total*p1Total)
-        })
-    })
-})
-
-console.log(Math.max(p1Wins,p2Wins)) // Part 2 answer
