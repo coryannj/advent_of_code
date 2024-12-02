70120dbe2071f62517042664ed5be3cd7b1e894f
Cory-Ann Joseph <coryannj@gmail.com>
2024-12-02 21:21:39 +1100

    D2 added for loop version

--- 2024/Day02.js
+++ 2024/Day02.js
@@ -11,3 +11,19 @@ let safe = arr.flatMap((x,ix)=> isSafe(x) ? [ix]:[])
 console.log(safe.length) // Part 1 answer
 
 console.log(arr.filter((x,ix)=>!safe.includes(ix) && x.some((y,yx)=> isSafe(x.toSpliced(yx,1)))).length + safe.length) // Part 2 answer
+
+// Version with for loop for fun
+let p1safe = 0;
+let p2safe = 0;
+
+for (i=0;i<arr.length;i++){
+    let x = arr[i];
+
+    if(isSafe(x)){
+        p1safe++
+    } else if (x.some((y,yx)=> isSafe(x.toSpliced(yx,1)))) {
+        p2safe++
+    }
+}
+console.log(arr.filter((x)=>[...new Set(x)].length<x.length))
+console.log('Part 1 answer is ',p1safe,' Part 2 answer is ',p1safe+p2safe)
