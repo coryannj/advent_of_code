605c34fad5b77905bd1f8ce363a104e689d951fb
Joseph, Cory-Ann <174399716+coryannj-anz@users.noreply.github.com>
2024-12-03 17:33:00 +1100

    Day 3 cleanup

--- 2024/Day03.js
+++ 2024/Day03.js
@@ -8,4 +8,4 @@ const dontRe = /don[']t[(][)][\s\S]+/
 
 console.log(input.match(mulRe).mk2d(',',1).map(x=>x[0]*x[1]).sum()) // Part 1 answer
 
-console.log(input.split(doRe).flatMap(x=>x.replace(dontRe,'').match(mulRe)??[]).mk2d(',',1).map(x=>x[0]*x[1]).sum()) // Part 2 answer
+console.log(input.split(doRe).map(x=>x.replace(dontRe,'').match(mulRe)?.mk2d(',',1).map(x=>x[0]*x[1]).sum()).sum()) // Part 2 answer
