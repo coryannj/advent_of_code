6a28a511d9bdf15765f74875d93521f32ea64c3f
Joseph, Cory-Ann <174399716+coryannj-anz@users.noreply.github.com>
2024-12-03 18:11:53 +1100

    D3 - version with eval for funsies

--- 2024/Day03.js
+++ 2024/Day03.js
@@ -9,3 +9,9 @@ const dontRe = /don[']t[(][)][\s\S]+/
 console.log(input.match(mulRe).mk2d(',',1).map(x=>x[0]*x[1]).sum()) // Part 1 answer
 
 console.log(input.split(doRe).flatMap(x=>x.replace(dontRe,'').match(mulRe)?.mk2d(',',1).map(x=>x[0]*x[1])).sum()) // Part 2 answer
+
+// Version with eval just for fun :-D
+
+console.log(eval(input.replaceAll(',','*').match(/(?<=mul)([(]\d+[*]\d+[)])/g).join('+')))
+
+console.log(eval(input.replaceAll(',','*').split(/do[(][)]/).map((x) => x.replace(/don[']t[(][)][\s\S]+/,'')).join('').match(/(?<=mul)([(]\d+[*]\d+[)])/g).join('+')))
