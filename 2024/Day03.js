201327d17f441044a10713b4c52cea5913496c39
Joseph, Cory-Ann <174399716+coryannj-anz@users.noreply.github.com>
2024-12-03 17:21:12 +1100

    Day 3

---
+++ 2024/Day03.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+require("../utils.js");
+const input = fs.readFileSync('../../day3.txt', {encoding: "utf8", flag: "r", });
+
+const mulRe = /(?<=mul[(])(\d+[,]\d+)(?=[)])/g
+const doRe = /do[(][)]/
+const dontRe = /don[']t[(][)][\s\S]+/
+
+console.log(input.match(mulRe).mk2d(',',1).map(x=>x[0]*x[1]).sum()) // Part 1 answer
+
+console.log(input.split(doRe).flatMap(x=>x.replace(dontRe,'').match(mulRe)??[]).mk2d(',',1).map(x=>x[0]*x[1]).sum()) // Part 2 answer
