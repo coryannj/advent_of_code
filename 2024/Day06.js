c8efe881405c9e3ee976e9625e0aefd593b39e95
Cory-Ann Joseph <coryannj@gmail.com>
2024-12-06 22:58:01 +1100

    Day 6 part 2

--- 2024/Day06.js
+++ 2024/Day06.js
@@ -1,22 +1,16 @@
 const fs = require("fs");
-require("../utils.js");
 const input = fs.readFileSync('./inputs/2024/day6.txt', {encoding: "utf8", flag: "r", });
 
-const a = input.lines().mk2d()
-//let b = structuredClone(a)
+const a = input.split(/[\r\n]+/).map((x)=>x.split(''));
 
-let startR = a.findIndex((x)=>x.includes('^'))
-let startC = a[startR].indexOf('^')
-let [rowLen,colLen] = [a.length,a[0].length]
-
-a[startR][startC] = '.'
-let start = [startR,startC,'U']
+// Part 1
+let startR = a.findIndex((x)=>x.includes('^'));
+let startC = a[startR].indexOf('^');
+a[startR][startC] = '.';
+let start = [startR,startC,'U'];
 
 const walk = (coOrd,partNo) => {
     let seen = new Set();
-    //let turns = new Set();
-    let steps = 0
-    let loopCount = 0
 
     const step = ([r,c,dir]) => {
         let nextObj = {
@@ -28,62 +22,44 @@ const walk = (coOrd,partNo) => {
 
         let turnObj =  {'R':'D','L':'U','U':'R','D':'L'}
 
-        let [nr,nc] = nextObj[dir]
+        let [nr,nc,nd] = nextObj[dir]
         let nextVal = a[nr]?.[nc]
 
         if(!nextVal){
             return undefined
-        } else if (nextVal === '#'){
-            //turns.add(`${[r,c].join('|')}`)
-            return nextObj[turnObj[dir]]
         } else {
-            return nextObj[dir]
-        }
-        
+            while(nextVal === '#'){
+                [nr,nc,nd] = nextObj[turnObj[nd]]
+                nextVal = a[nr][nc]
+            }
+            return [nr,nc,nd]
+        }        
     }
 
     while(coOrd !== undefined){
-        //if(seen.has(coOrd.join('|'))) loopCount++;
         if(partNo === 2 && seen.has(coOrd.join('|'))) break;
         partNo === 1 ? seen.add(coOrd.slice(0,2).join('|')) : seen.add(coOrd.join('|'))
-
-
         coOrd = step(coOrd);
-        
-        steps++
     }
     
     if(coOrd === undefined){
-        return partNo === 1 ? [seen.size,steps,seen] : 0
+        return partNo === 1 ? [seen.size,seen] : 0
     } else {
-        console.log(coOrd,steps,loopCount,seen.size)
         return 1
     }
 }
 
-//console.log(walk(start,1))
+let [p1,p1path] = walk(start,1)
 
-let [p1ans,p1steps,p1path] = walk(start,1)
-
-console.log(p1ans)
-
-
-
-
+// Part 2
+p1path.delete(start.slice(0,2).join('|'))
 let p2 = 0
 
-for(i=0;i<rowLen;i++){
-    for(j=0;j<colLen;j++){
-        if((i===startR && j === startC) || a[i][j] === '#'){
-            continue;
-        } else {
-            a[i][j] = '#'
-            p2 += walk(start,2)
-
-            a[i][j] = '.'
-        }
-    }
+for(const item of p1path){
+    let [r,c] = item.split('|').map(Number);
+    a[r][c] = '#';
+    p2+=walk(start,2);
+    a[r][c] = '.';
 }
 
-
-console.log(p2)
+console.log(p1,p2)
