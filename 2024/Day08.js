c00a86d84553ae23ef4235d5135e5a1d2997618a
Cory-Ann Joseph <coryannj@gmail.com>
2024-12-08 19:55:45 +1100

    Day 8 bugfix

--- 2024/Day08.js
+++ 2024/Day08.js
@@ -25,26 +25,26 @@ let p2 = new Set()
 for(const[k,v] of Object.entries(antennas)){
     while(v.length>1){
         let [r1,c1] = v.shift()
+        p2.add(`${r1}_${c1}`);
 
         for(l=0;l<v.length;l++){
             let [r2,c2] = v[l];
+            p2.add(`${r2}_${c2}`);
             let [rdiff,cdiff] = [r1-r2,c1-c2];
             let steps = 1;
 
-            p2.add(`${r1}_${c1}`);
-            p2.add(`${r2}_${c2}`);
-
             while(true){
                 let changed = 0
-                let [nr1,nc1,nr2,nc2] = [r1+(rdiff*steps),c1+(cdiff*steps),r2-(rdiff*steps),c2-(cdiff*steps)]
+                let [nr1,nc1] = [r1+(rdiff*steps),c1+(cdiff*steps)]
+                let [nr2,nc2] = [r2-(rdiff*steps),c2-(cdiff*steps)]
 
-                if(0 <= nr1 && nr1 < rowlen && 0 <= nc1 && nc1 <=collen){
+                if(0 <= nr1 && nr1 < rowlen && 0 <= nc1 && nc1 < collen){
                     if(steps === 1) p1.add(`${nr1}_${nc1}`)
                     p2.add(`${nr1}_${nc1}`)
                     changed++
                 }
 
-                if(0 <= nr2 && nr2 < rowlen && 0 <= nc2 && nc2 <=collen){
+                if(0 <= nr2 && nr2 < rowlen && 0 <= nc2 && nc2 < collen){
                     if(steps === 1 ) p1.add(`${nr2}_${nc2}`)
                     p2.add(`${nr2}_${nc2}`)
                     changed++
