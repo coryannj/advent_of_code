40896af513381996b467741a8eb249a45b10d5dc
Cory-Ann Joseph <coryannj@gmail.com>
2024-12-10 22:52:17 +1100

    Added grid util and perf marks

--- 2024/Day10.js
+++ 2024/Day10.js
@@ -8,27 +8,25 @@ let queue = grid.flat().filter((x)=>x[2]===0).map((x)=>[x])
 let p1 = Object.fromEntries(queue.map((x)=>[x.flat().join('|'),new Set()]))
 let p2 = new Set()
 
-let [r,c,val] = queue[0][0]
-console.log(r,c,val)
-const nextTest = (([nr,nc]) => grid[nr]?.[nc]?.[2]===val+1)  
-
-//const cumulativeSum = (sum => value => sum += value)(0);
-
-console.log(nextArr([r,c],grid,nextTest))
+let t0 = performance.now()
 
 while(queue.length>0){
     let next = queue.shift()
     let [r,c,val] = next.at(-1)
+    const nextTest = (([nr,nc]) => grid[nr]?.[nc]?.[2]===val+1)  
 
     if(val === 9){
        p1[next[0].join('|')].add([r,c,val].join('|'))
        p2.add(next.join('|'))
     } else {
-        [[r,c+1],[r,c-1],[r+1,c],[r-1,c]].filter(([nr,nc])=> grid[nr]?.[nc]?.[2]===val+1).forEach(([nr,nc])=>{
+        nextArr([r,c],grid,nextTest).forEach(([nr,nc])=>{
             queue.push(next.concat([grid[nr][nc]]))
         })
     }    
 }
 
-console.log(Object.values(p1).map((x)=>x.size).sum())
-console.log(p2.size)
+let t1 = performance.now()
+
+console.log(Object.values(p1).map((x)=>x.size).sum()) // P1 answer
+console.log(p2.size) // P2 answer
+console.log(t1-t0) // runtime
