90015458f0717074f2236c81aef572da72c2c873
Joseph, Cory-Ann <174399716+coryannj-anz@users.noreply.github.com>
2024-12-10 17:43:40 +1100

    Day 10

---
+++ 2024/Day10.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+require("../utils.js");
+const input = fs.readFileSync('./inputs/2024/day10.txt', {encoding: "utf8", flag: "r", });
+const grid = input.lines().mk2d('',1).map((x,ix)=>x.map((y,yx)=>[ix,yx,y]))
+let queue = grid.flat().filter((x)=>x[2]===0).map((x)=>[x])
+
+let p1 = Object.fromEntries(queue.map((x)=>[x.flat().join('|'),new Set()]))
+let p2 = new Set()
+
+while(queue.length>0){
+    let next = queue.shift()
+    let [r,c,val] = next.at(-1)
+
+    if(val === 9){
+       p1[next[0].join('|')].add([r,c,val].join('|'))
+       p2.add(next.join('|'))
+    } else {
+        [[r,c+1],[r,c-1],[r+1,c],[r-1,c]].filter(([nr,nc])=> grid[nr]?.[nc]?.[2]===val+1).forEach(([nr,nc])=>{
+            queue.push(next.concat([grid[nr][nc]]))
+        })
+    }    
+}
+
+console.log(Object.values(p1).map((x)=>x.size).sum())
+console.log(p2.size)
