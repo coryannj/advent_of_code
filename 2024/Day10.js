e018fdd90acf8c358bf6b02597e2a5270d0d3ec2
Cory-Ann Joseph <coryannj@gmail.com>
2024-12-10 22:42:38 +1100

    Utils

--- 2024/Day10.js
+++ 2024/Day10.js
@@ -1,11 +1,20 @@
 const fs = require("fs");
 require("../utils.js");
+const { nextArr } = require("../utils.js");
 const input = fs.readFileSync('./inputs/2024/day10.txt', {encoding: "utf8", flag: "r", });
 const grid = input.lines().mk2d('',1).map((x,ix)=>x.map((y,yx)=>[ix,yx,y]))
 let queue = grid.flat().filter((x)=>x[2]===0).map((x)=>[x])
 
 let p1 = Object.fromEntries(queue.map((x)=>[x.flat().join('|'),new Set()]))
 let p2 = new Set()
+
+let [r,c,val] = queue[0][0]
+console.log(r,c,val)
+const nextTest = (([nr,nc]) => grid[nr]?.[nc]?.[2]===val+1)  
+
+//const cumulativeSum = (sum => value => sum += value)(0);
+
+console.log(nextArr([r,c],grid,nextTest))
 
 while(queue.length>0){
     let next = queue.shift()
