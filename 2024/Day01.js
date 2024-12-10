67d5a24a0c3e384799aab3a9bfeb24c68afed2eb
Cory-Ann Joseph <coryannj@gmail.com>
2024-12-10 19:12:31 +1100

    Day 1 added golf version for p1

--- 2024/Day01.js
+++ 2024/Day01.js
@@ -2,8 +2,13 @@ const fs = require('fs');
 require('../utils.js');
 
 const input = fs.readFileSync('../../day1.txt',{ encoding: 'utf8', flag: 'r' });
-//const inputE = fs.readFileSync('../2024/day1example.txt',{ encoding: 'utf8', flag: 'r' });
 
+// Part 1 golf version
+let t=input
+let [l,r]=[/^\d+/gm,/\d+$/gm].map(x=>t.match(x).sort());a=l.reduce((a,c,i)=>a+Math.abs(+c-+r[i]),0)
+console.log(a)
+
+// Non golf version
 let arr = input.lines().mk2d('\\s+',1)
 let col1 = arr.col(0).sorta()
 let col2 = arr.col(1).sorta()
