34fdcf8d098fa5778fb745e47da07174bc869904
Cory-Ann Joseph <coryannj@gmail.com>
2024-12-20 00:18:17 +1100

    Day 16 rewrite

--- 2024/Day16.js
+++ 2024/Day16.js
@@ -8,11 +8,71 @@ let startR = grid.findIndex((x)=>x.includes('S'));
 let startC = grid[startR].indexOf('S');
 let endR = grid.findIndex((x)=>x.includes('E'));
 let endC = grid[endR].indexOf('E');
+let rowLen = grid.length
+let colLen = grid[0].length
+let rowFactor = 1
+let colFactor = 1
 
-const nextStep = ([sr,sc,dir]) => {
-    let backwards = {U:'D',D:'U',L:'R',R:'L'}
-    return [[sr-1,sc,'U'],[sr,sc+1,'R'],[sr+1,sc,'D'],[sr,sc-1,'L']].filter(([nr,nc,nd])=>grid[nr][nc] !== '#' && nd !== backwards[dir])
+while(rowFactor<=colFactor*colLen){
+    rowFactor*=10
 }
+
+// Helper functions
+const rcToNum = (r,c) => (r*rowFactor)+(c*colFactor)
+const numToRC = (num) => {
+    if(num<rowFactor) return [0,num]
+    return [(num-(num%rowFactor))/rowFactor,num%rowFactor]
+}
+const getNum = (num) => {
+    let c = num%rowFactor
+    let r = (num-c)/rowFactor
+    return grid[r][c]
+}
+const rcStep = (num,factor,[nr,nc]) => {
+    let r,c
+    if (num<factor){
+        r = 0,c = num
+    } else {
+        c = num%factor
+        r = (num-c)/factor
+    }
+
+    return rcToNum([r+nr,c+nc],factor)
+}
+
+let gridMap = new Map(grid.flatMap((x,ix)=>x.map((y,yx)=>[rcToNum(ix,yx),y])).filter(([k,v])=>'.SE'.includes(v)))
+console.log([...gridMap.keys()].slice(-100))
+
+console.log([...gridMap.keys()].filter((x)=>gridMap.get(x) === 'S'||gridMap.get(x)==='E'))
+console.log(rcToNum(startR,startC),rcToNum(endR,endC))
+
+let start = rcToNum(startR,startC)
+let end = rcToNum(endR,endC)
+
+console.log(rcToNum(-1,1),rcToNum(-1,0))
+
+let distVal = {
+    U:1000000,
+    R:2000000,
+    D:3000000,
+    L:4000000
+}
+
+// {score,seen,prev,last,currDir}
+// colfactor +/-
+// rowfactor +/-
+
+
+
+
+
+
+
+
+
+
+
+
 
 let queue = Array(2000).fill('.').map((x)=>[])
 queue[0].push([0,[[startR,startC,'R']]])
@@ -21,6 +81,25 @@ let p1min = 99999999
 let p2 = []
 
 let distObj = Object.fromEntries(grid.flatMap((x,ix)=>x.flatMap((y,yx)=>['U','D','L','R'].map((z)=>[`${ix}|${yx}|${z}`,999999]))))
+
+let nextObj = {}
+let cacheHits = 0
+const nextStep = ([sr,sc,dir]) => {
+    if(nextObj[[sr,sc,dir].join('|')] !== undefined){
+        cacheHits++
+        return nextObj[[sr,sc,dir].join('|')]
+    } 
+
+    let backwards = {U:'D',D:'U',L:'R',R:'L'}
+    
+    let next = [[sr-1,sc,'U'],[sr,sc+1,'R'],[sr+1,sc,'D'],[sr,sc-1,'L']].filter(([nr,nc,nd])=>grid[nr][nc] !== '#' && nd !== backwards[dir])
+    nextObj[[sr,sc,dir].join('|')] = next
+    
+    return next
+}
+
+
+let t0=performance.now()
 
 while(queue.some((x)=>x.length>0)){
 
@@ -50,7 +129,8 @@ while(queue.some((x)=>x.length>0)){
         }
     })
 }
-
+let t1=performance.now()
 let allP2MinPaths = new Set(p2.filter((x)=>x[0]===p1min).flatMap((x)=>x[1]).map((x)=>x.slice(0,2).join('|')))
-
-console.log('P1 answer is ',p1min,' P2 answer is ',allP2MinPaths.size)
+let t2 = performance.now()
+console.log('P1 answer is ',p1min,t1-t0,' P2 answer is ',allP2MinPaths.size,t2-t1,t2-t0)
+console.log(cacheHits)
