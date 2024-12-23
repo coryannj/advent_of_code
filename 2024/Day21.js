928046815656f1ae73bf6c8ba7d1bbace486d27c
Cory-Ann Joseph <coryannj@gmail.com>
2024-12-23 19:55:48 +1100

    Day 23

--- 2024/Day21.js
+++ 2024/Day21.js
@@ -5,6 +5,67 @@ const { codes } = require('../inputs/2024/day21.js') // Array of hardcoded numer
 const input = fs.readFileSync('../inputs/2024/day21.txt', {encoding: "utf8", flag: "r", });
 const keypadCodes = input.lines();
 
+const keypad = [
+    ['7', '8', '9'],
+    ['4', '5', '6'],
+    ['1', '2', '3'],
+    [null, '0', 'A'],
+]
+
+
+
+
+const allShortest = (start,grid) => {
+    let pathsObj = {}
+    let seen = new Set()
+    seen.add(start.join('|'))
+    let startVal = grid[start[0]][start[1]]
+    let queue = [[[start,'',startVal]]]
+
+    const nextStep = ([r,c],grid) => {
+        return [[r-1,c,'^'],[r,c+1,'>'],[r+1,c,'v'],[r,c-1,'<']].filter(([nr,nc,nd])=>grid?.[nr]?.[nc] !== undefined && !seen.has(`${nr}|${nc}`) && !!grid?.[nr]?.[nc]).map(([nr,nc,nd])=>[[nr,nc],nd].concat(grid[nr][nc]))
+    }
+    //let next = nextStep(start,grid)
+    
+    console.log('queue is ',queue)
+
+    while(queue.length>0){
+        let nextQueue = []
+        let queueSeen = []
+
+        queue.forEach((path)=>{
+            //console.log('queue x is ',path)
+            let last = [point,dir,val] = path.at(-1)
+            //console.log('last is ',last)
+
+            let next = nextStep(point,grid)
+            //console.log('next is ',next)
+
+            next.forEach(([np,nd,nv])=>{
+                queueSeen.push(np.join('|'))
+                let newPath = path.concat([[np,nd,nv]])
+                pathsObj[nv] ? pathsObj[nv].push(newPath) : pathsObj[nv] = [newPath]
+                //console.log('pathobj',nv,' is ',pathsObj[nv])
+                nextQueue.push(newPath)
+//newCountObj[newKey] ? newCountObj[newKey]+=val : newCountObj[newKey] = val
+
+            })
+            
+        })
+        queueSeen.forEach((x)=>seen.add(x))
+        queue = nextQueue
+        console.log('queue is now ',queue)
+        console.log('seen is now ',seen)
+    }
+
+    return pathsObj
+}
+
+let pathsObj = allShortest([3,2],keypad)
+
+Object.keys(pathsObj).forEach((x)=>console.log('x is ',x,pathsObj[x]))
+
+
 const dirs = {
     '<':'v<<A',
     '^':'<A',
---
+++ 2024/Day23.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+require("../utils.js");
+const input = fs.readFileSync('../inputs/2024/day23.txt', {encoding: "utf8", flag: "r", });
+
+const cMap = input.lines().mk2d('-')
+
+// Create adjacency list
+let cObj = Object.fromEntries([...new Set(cMap.flat().map((x)=>[x,new Set()]))])
+
+cMap.forEach(([c1,c2])=>{
+    cObj[c1].add(c2)
+    cObj[c2].add(c1)
+})
+
+// Part 1
+let t0 = performance.now()
+let cSets = new Set()
+let chiefs = [...Object.entries(cObj).filter(([k,v])=>k[0]==='t')]
+
+chiefs.forEach(([k0,v0])=>{
+    let connected = [...v0.values()]
+
+    while(connected.length>0){
+        let k1 = connected.shift()
+        let v1 = cObj[k1]
+
+        connected.forEach((k2)=>{
+            let v2 = cObj[k2]
+            if(v1.has(k2) && v2.has(k1)){
+                cSets.add([k0,k1,k2].sort().join('|'))
+            }
+        })
+
+    }
+})
+
+let p1 = cSets.size
+let t1 = performance.now()
+
+console.log('Part 1 answer is ',p1,t1-t0,'ms')
+
+// Part 2
+let t2 = performance.now()
+let p2 = Object.entries(cObj).map(([k1,v1])=>
+            Object.entries(
+                [...v1].map((k2)=>
+                    [...cObj[k1].intersection(cObj[k2])].concat(k1,k2).sort().join(',')).reduce((a, c) => {return a[c] ? ++a[c] : a[c] = 1, a}, {})
+                ).filter(([intKey,intCount])=>
+                    intKey.split(',').length === intCount+1
+                ).sort((a,c)=>c[1]-a[1])[0]
+         ).sort((a,c)=>c[1]-a[1])[0][0]
+let t3 = performance.now()
+
+console.log('Part 2 answer is ',p2,t3-t2,'ms')
