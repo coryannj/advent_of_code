eeca03d6f5bd68d01447822b37ff106198465593
Cory-Ann Joseph <coryannj@gmail.com>
2025-12-05 19:10:37 +1100

    lol remembered Array.with() exists :-D

--- 2025/Day05.js
+++ 2025/Day05.js
@@ -5,14 +5,9 @@ const input = fs.readFileSync('../inputs/2025/day5.txt', {encoding: "utf8", flag
 let [ranges,ids] = input.split(/\n\n/).map((x,i)=> i === 0 ? x.split(/\n/).map((y)=>y.split('-').map(Number)).sort((a,b)=> a[0]===b[0] ? a[1]-b[1] : a[0]-b[0]) : x.split(/\n/).map(Number))
 
 const mergeSortedRanges = (arr,[x2,y2]) => {
-    let y1 = arr.at(-1)[1]
+    if(y2 <= arr.at(-1)[1]) return arr;
 
-    if(y2 <= y1) return arr;
-    if(x2 > y1) return arr.concat([[x2,y2]]);
-    
-    arr.at(-1)[1] = y2
-
-    return arr
+    return x2 > arr.at(-1)[1] ? arr.concat([[x2,y2]]) : arr.with(-1,[arr.at(-1)[0],y2])
 }
 
 let p1 = ids.filter((x)=> ranges.some(([s,e])=> x >= s && x <= e)).length
