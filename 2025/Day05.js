34ec4b981bbf3721bd8a1da65dcc089da2b3119e
coryannj-anz <174399716+coryannj-anz@users.noreply.github.com>
2025-12-05 18:02:26 +1100

    Day 5 solution

---
+++ 2025/Day05.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+require('../utils.js');
+const input = fs.readFileSync('../inputs/2025/day5.txt', {encoding: "utf8", flag: "r", });
+
+let [ranges,ids] = input.split(/\n\n/).map((x,i)=> i === 0 ? x.split(/\n/).map((y)=>y.split('-').map(Number)) : x.split(/\n/).map(Number))
+
+const mergeSortedRanges = (arr,[x2,y2]) => {
+    let y1 = arr.at(-1)[1]
+
+    if(y2 <= y1) return arr;
+    if(x2 > y1) return arr.concat([[x2,y2]]);
+    
+    arr.at(-1)[1] = y2
+
+    return arr
+}
+
+let p1 = ids.filter((x)=> ranges.some(([s,e])=> x >= s && x <= e)).length
+
+let p2 = ranges
+            .sort((a,b)=> a[0]===b[0] ? a[1]-b[1] : a[0]-b[0])
+            .reduce((a,c)=> mergeSortedRanges(a,c),[ranges.shift()])
+            .reduce((a,c)=> a + (c[1]-c[0]+1), 0)
+
+console.log('Part 1 answer is ', p1)
+console.log('Part 2 answer is ', p2)
