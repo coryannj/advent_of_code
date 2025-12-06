cfeaec900939a1003478a0c39bb875085971b0c5
Cory-Ann Joseph <coryannj@gmail.com>
2025-12-06 18:37:58 +1100

    Day 6 solution

---
+++ 2025/Day06.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+require('../utils.js');
+const input = fs.readFileSync('../inputs/2025/day6.txt', {encoding: "utf8", flag: "r", });
+
+let
+    lines = input.split(/[\r\n]+/),
+    nums = lines.slice(0,-1),
+    opLine = lines.at(-1),
+    operators = [...opLine.matchAll(/\S/g)].map((x)=>[x[0],x.index]),
+    colLen = operators.length,
+    lastMatchInd = 0,
+    p1 = 0,
+    p2 = 0
+
+operators.forEach(([opVal,matchInd],colInd,a)=>{
+    let nextMatchInd = colInd < colLen-1 ? a[colInd+1][1] : colLen+1
+    let p1Col = colInd < colLen-1 ? nums.map((x)=>x.slice(lastMatchInd,nextMatchInd-1)) : nums.map((x)=>x.slice(lastMatchInd))
+
+    let p2Col = Array.from({ length: p1Col[0].length }, (_, i) => p1Col.map((x)=>x[i]).join(''))
+
+    p1+=(eval(p1Col.join(opVal)))
+    p2+=(eval(p2Col.join(opVal)))
+
+    lastMatchInd = nextMatchInd
+})
+
+console.log(p1)
+console.log(p2)
