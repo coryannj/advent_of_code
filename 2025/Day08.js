779feee247ba16816429a2088553e00998aa11e3
Cory-Ann Joseph <coryannj@gmail.com>
2025-12-08 22:37:07 +1100

    Day 8 tidy up - still not happy Jan

--- 2025/Day08.js
+++ 2025/Day08.js
@@ -7,31 +7,31 @@ const getDistance = ([x1,y1,z1],[x2,y2,z2])=> Math.pow((x1-x2),2) + Math.pow((y1
 let
     lines = input.split(/[\r\n]+/).map((x,xi)=>[xi,x.split(',').map(Number)]),
     circuits = Array(lines.length).fill('.').map((x,xi)=>new Set([xi])),
+    cLen = circuits.length,
     target = 1000,
     counter = 0,
     p1,
     p2
 
-// Pre-calculate all distances, then sort - key is dist, value is set of indices
-let preDistances = new Map()   
+// Pre-calculate all distances, key is dist, value is set of indices
+let distances = {} 
+
 lines.values().forEach(([xi,xv])=>{
-    let rest = lines.filter((l,li)=>li>xi && li !== xi)
+    let rest = lines.filter((l,li)=> li > xi)
 
     rest.values().forEach(([ri,rv])=>{
-        preDistances.set(getDistance(xv,rv),new Set([xi,ri]))
+        distances[getDistance(xv,rv)] = new Set([xi,ri])
     })
 })
 
-let sortedDistances = new Map([...preDistances.entries()].sort((a,b)=>a[0]-b[0]))
+for(const c of Object.values(distances)){
 
-for(const c of sortedDistances.values()){
-    
     // Part 1 answer
-    if(counter === target) p1 = circuits.filter((x)=>x !== null).map((x)=>x.size).sort((a,b)=>b-a).slice(0,3).reduce((a,c)=>a*c,1)
-        
+    if(counter === target) p1 = circuits.map((x)=>x.size).sort((a,b)=>b-a).slice(0,3).reduce((a,c)=>a*c,1)
+
     counter++
     
-    let cInds = [...c].map((x)=>circuits.findIndex((y)=> y!==null && y.has(x)))
+    let cInds = [...c].map((x)=>circuits.findIndex((y)=> y.has(x)))
     
     if(cInds[0] === cInds[1]) continue;
 
@@ -39,10 +39,11 @@ for(const c of sortedDistances.values()){
     let cMax = cMin === cInds[0] ? cInds[1] : cInds[0]
 
     circuits[cMin] = circuits[cMin].union(circuits[cMax])
-    circuits[cMax] = null
+    circuits.splice(cMax,1)
+    cLen--
 
     // Part 2 answer
-    if(!circuits.some((x)=>x !== null && x.size === 1)){
+    if(cLen === 1){
         p2 = [...c].map((x)=>lines[x][1]).map((x)=> x[0]).reduce((a,c)=>a*c,1)
         break;
     }
