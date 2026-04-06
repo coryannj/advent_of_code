7c52d75eed61090ef27dc3d134dd2f8e8b595848
Cory-Ann Joseph <coryannj@gmail.com>
2026-04-07 03:33:12 +1000

    AOC 2017 d10

---
+++ 2017/Day10.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+require('../inputs/utils.js');
+const input = fs.readFileSync('../inputs/2017/day10.txt', {encoding: "utf8", flag: "r", });
+
+const solve = (partNo) => {
+    let 
+        lengths = partNo === 1 ? input.split(',').map(Number) : input.split('').map((x)=>x.charCodeAt(0)).concat([17, 31, 73, 47, 23]),
+        listLen = 256,
+        list = Array(listLen).fill().map((x,i)=>i),
+        rounds = partNo === 1 ? 1 : 64,
+        currPos = 0,
+        skipSize = 0
+    
+    while(rounds--){
+        for(const l of lengths){
+            let subList = list.splice(currPos,l)
+
+            if(subList.length<l){
+                let endLen = subList.length
+                let startLen = l-endLen
+                subList = subList.concat(list.splice(0,startLen)).reverse()
+                list.splice(currPos,0,...subList.splice(0,endLen))
+                list.splice(0,0,...subList)
+            } else {
+                list.splice(currPos,0,...subList.reverse())
+            }
+
+            currPos = (currPos + l + skipSize) % listLen
+            skipSize++
+        }
+    }
+
+return partNo === 1 ? list[0]*list[1] : list.chunks(16).map((x)=>(x.reduce((a,c)=>a^c)).toString(16).padStart(2,'0')).join('')
+
+}
+
+console.log(solve(1))
+console.log(solve(2))
