e5653705323135c28121cfc5d69d11ab640622ce
Cory-Ann Joseph <coryannj@gmail.com>
2026-04-07 02:41:29 +1000

    Day 17 cleanup

--- 2017/Day17.js
+++ 2017/Day17.js
@@ -4,24 +4,21 @@ require('../inputs/utils.js');
 
 const solve = (partNo) => {
     const input = 371
-    
+
     let
         rounds = partNo === 1 ? 2017 : 50000000,
         spinlock = {0:0},
-        currVal = 1,
         currPos = 0
     
-    do {
-        for(i=0;i<input;i++){
+    for(i=1;i<=rounds;i++){
+        for(j=0;j<input;j++){
             currPos = spinlock[currPos]
         }
 
-        spinlock[currVal] = spinlock[currPos]
-        spinlock[currPos] = currVal
-        
-        currPos = currVal
-        currVal++
-    } while(currVal<=rounds)
+        spinlock[i] = spinlock[currPos]
+        spinlock[currPos] = i
+        currPos = i
+    }
 
     return spinlock[partNo === 1 ? 2017 : 0]
 }
